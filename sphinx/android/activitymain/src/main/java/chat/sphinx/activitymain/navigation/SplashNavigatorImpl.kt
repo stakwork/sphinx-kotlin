737e1fb674b2e2e2af60cca5e7ab19fdb41ef845
Matthew Nelson <developer@matthewnelson.io>
2021-02-23 13:49:27 -0500

    break out navigators into new viewmodel scoped module

--- sphinx/android/activitymain/src/main/java/chat/sphinx/activitymain/navigation/SplashNavigatorImpl.kt
+++ sphinx/android/activitymain/src/main/java/chat/sphinx/activitymain/navigation/SplashNavigatorImpl.kt
@@ -1,8 +1,10 @@
 package chat.sphinx.activitymain.navigation
 
 import chat.sphinx.splash.navigation.SplashNavigator
+import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
+@ViewModelScoped
 class SplashNavigatorImpl @Inject constructor(
     navigationDriver: MainNavigationDriver
 ): SplashNavigator(navigationDriver) {
