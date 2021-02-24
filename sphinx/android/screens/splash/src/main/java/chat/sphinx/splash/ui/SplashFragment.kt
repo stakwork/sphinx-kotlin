9af5c6eabba76ef324dca710c1b275479c42cdf3
Matthew Nelson <developer@matthewnelson.io>
2021-02-24 07:55:24 -0500

    remove setSpaceHeight method and add viewModel

--- sphinx/android/screens/splash/src/main/java/chat/sphinx/splash/ui/SplashFragment.kt
+++ sphinx/android/screens/splash/src/main/java/chat/sphinx/splash/ui/SplashFragment.kt
@@ -1,10 +1,9 @@
 package chat.sphinx.splash.ui
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
-import android.widget.Space
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import chat.sphinx.splash.R
 import chat.sphinx.splash.databinding.FragmentSplashBinding
@@ -14,31 +13,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class SplashFragment: Fragment(R.layout.fragment_splash) {
 
     private val binding: FragmentSplashBinding by viewBinding(FragmentSplashBinding::bind)
+    private val viewModel: SplashViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setSpaceHeight(binding.spaceSplashStatusBar)
-    }
-
-    /**
-     * Have to programmatically set height of the space to the height of the status bar
-     * for the device so the image view does not move at startup.
-     * */
-    private fun setSpaceHeight(space: Space) {
-        requireActivity().apply {
-            val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
-            val height = if (resourceId > 0) {
-                resources.getDimensionPixelSize(resourceId)
-            } else {
-                Rect().apply {
-                    window.decorView.getWindowVisibleDisplayFrame(this)
-                }.top
-            }
-
-            val params = space.layoutParams.apply {
-                this.height = height
-            }
-            space.layoutParams = params
-        }
     }
 }
