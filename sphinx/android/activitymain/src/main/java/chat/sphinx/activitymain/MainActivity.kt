28935ffe984a5420b89572ff160605d458612e36
Matthew Nelson <developer@matthewnelson.io>
2021-02-24 07:51:30 -0500

    implement insetter library for MainActivity

--- sphinx/android/activitymain/src/main/java/chat/sphinx/activitymain/MainActivity.kt
+++ sphinx/android/activitymain/src/main/java/chat/sphinx/activitymain/MainActivity.kt
@@ -1,11 +1,14 @@
 package chat.sphinx.activitymain
 
+import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import chat.sphinx.activitymain.databinding.ActivityMainBinding
 import chat.sphinx.activitymain.navigation.MainNavigationDriver
+import chat.sphinx.resources.R as R_common
 import dagger.hilt.android.AndroidEntryPoint
 import io.matthewnelson.android_feature_activity.NavigationActivity
 import io.matthewnelson.concept_navigation.NavigationRequest
@@ -25,6 +28,12 @@ class MainActivity: NavigationActivity<
     override val viewModel: MainViewModel by viewModels()
     override val navigationViewModel: MainViewModel
         get() = viewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R_common.style.AppPostLaunchTheme)
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
 
     override suspend fun onPostNavigationRequestExecution(request: NavigationRequest<NavController>) {
         super.onPostNavigationRequestExecution(request)
