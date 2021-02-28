71a94005396283bd0c399fd980fecd10c7e396fb
Matthew Nelson <developer@matthewnelson.io>
2021-02-28 09:42:44 -0500

    temporarily remove annotation for main driver

--- sphinx/android/activitymain/src/main/java/chat/sphinx/activitymain/di/NavigationModules.kt
+++ sphinx/android/activitymain/src/main/java/chat/sphinx/activitymain/di/NavigationModules.kt
@@ -3,7 +3,6 @@ package chat.sphinx.activitymain.di
 import androidx.navigation.NavController
 import chat.sphinx.activitymain.navigation.MainNavigationDriver
 import chat.sphinx.activitymain.navigation.SplashNavigatorImpl
-import chat.sphinx.annotation_hilt.navigation.MainDriver
 import chat.sphinx.splash.navigation.SplashNavigator
 import dagger.Module
 import dagger.Provides
@@ -17,7 +16,6 @@ import io.matthewnelson.concept_navigation.BaseNavigationDriver
 object ActivityRetainedModule {
 
     @Provides
-    @MainDriver
     fun provideMainNavigationDriver(
         mainNavigationDriver: MainNavigationDriver
     ): BaseNavigationDriver<NavController> =
