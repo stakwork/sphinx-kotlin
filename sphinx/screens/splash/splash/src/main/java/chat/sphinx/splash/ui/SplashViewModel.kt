5e0eb5016ad824123d86912aeffea9087e1998ee
Matthew Nelson <developer@matthewnelson.io>
2021-03-02 17:09:07 -0500

    implement BackgroundLoginHandler in SplashScreen

--- sphinx/screens/splash/splash/src/main/java/chat/sphinx/splash/ui/SplashViewModel.kt
+++ sphinx/screens/splash/splash/src/main/java/chat/sphinx/splash/ui/SplashViewModel.kt
@@ -3,29 +3,33 @@ package chat.sphinx.splash.ui
 import android.content.Context
 import android.os.SystemClock
 import androidx.lifecycle.viewModelScope
+import chat.sphinx.background_login.BackgroundLoginHandler
 import chat.sphinx.splash.navigation.SplashNavigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.matthewnelson.android_feature_viewmodel.MotionLayoutViewModel
 import io.matthewnelson.android_feature_viewmodel.submitSideEffect
 import io.matthewnelson.android_feature_viewmodel.updateViewState
 import io.matthewnelson.concept_authentication.coordinator.AuthenticationCoordinator
-import io.matthewnelson.concept_authentication.data.AuthenticationStorage
+import io.matthewnelson.concept_authentication.coordinator.AuthenticationRequest
+import io.matthewnelson.concept_authentication.coordinator.AuthenticationResponse
 import io.matthewnelson.concept_coroutines.CoroutineDispatchers
 import io.matthewnelson.concept_views.viewstate.ViewStateContainer
 import io.matthewnelson.k_openssl_common.extensions.decodeToString
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import okio.base64.decodeBase64ToArray
 import org.cryptonode.jncryptor.AES256JNCryptor
 import org.cryptonode.jncryptor.CryptorException
+import javax.annotation.meta.Exhaustive
 import javax.inject.Inject
 
 @HiltViewModel
 internal class SplashViewModel @Inject constructor(
     private val dispatchers: CoroutineDispatchers,
     private val authenticationCoordinator: AuthenticationCoordinator,
-    private val authenticationStorage: AuthenticationStorage,
+    private val backgroundLoginHandler: BackgroundLoginHandler,
     private val navigator: SplashNavigator
 ): MotionLayoutViewModel<
         Any,
@@ -42,29 +46,35 @@ internal class SplashViewModel @Inject constructor(
             screenInit = true
         }
 
-        val initTime = SystemClock.uptimeMillis()
-        // check for credentials from authentication prefs
-        // TODO: ask if private mode persists across app restart. If so, overwrite creds with
-        //  "PRIVATE_MODE" value on logout to ensure re-logging in always
-        //  requires a pin for private mode
-
-        // TODO: Build API for authentication coordinator/manager to check if credentials are set
-            // if background logout creds not null (we know user account exists)
-                // if time last login - initTime > logout setting
-                    // remove from storage
-                    // request login
-                // else
-                    // re-save with new time
-                    // send login request with key (TODO: Build API out for authentication coordinator/manager)
-            // else (we don't know if user account exists)
-                // send request to see if encryption key exists in storage (TODO: Build API for authentication coordinator/manager)
-                    // if exists
-                        // request encryption key (auto navigate to login)
-                        // if user setting for background logout is not 0
-                            // save key with new time
-                        // navigate to home screen
-                    // if doesn't exist
-                        // start onboard anim
+        viewModelScope.launch(dispatchers.mainImmediate) {
+            backgroundLoginHandler.attemptBackgroundLogin()?.let {
+                navigator.toHomeScreen()
+            } ?: let {
+                if (authenticationCoordinator.isAnEncryptionKeySet()) {
+                    authenticationCoordinator.submitAuthenticationRequest(
+                        AuthenticationRequest.LogIn()
+                    ).first().let { response ->
+                        @Exhaustive
+                        when (response) {
+                            is AuthenticationResponse.Failure -> {
+                                // will not be returned as back press for handling
+                                // a LogIn request minimizes the application until
+                                // User has authenticated
+                            }
+                            is AuthenticationResponse.Success.Authenticated -> {
+                                navigator.toHomeScreen()
+                            }
+                            is AuthenticationResponse.Success.Key -> {
+                                // will never be returned
+                            }
+                        }
+                    }
+                } else {
+                    // Display OnBoard
+                    updateViewState(SplashViewState.StartScene)
+                }
+            }
+        }
     }
 
     // TODO: Limit to not cause buffer overflow and crash.
@@ -158,15 +168,6 @@ internal class SplashViewModel @Inject constructor(
                 exception.printStackTrace()
                 submitSideEffect(SplashSideEffect.DecryptionFailure)
             }
-        }
-    }
-
-    // TODO: Temporary until Authentication gets built out.
-    init {
-        viewModelScope.launch(dispatchers.mainImmediate) {
-            // need a slight delay for window hand over
-            delay(375L)
-            updateViewState(SplashViewState.StartScene)
         }
     }
 
