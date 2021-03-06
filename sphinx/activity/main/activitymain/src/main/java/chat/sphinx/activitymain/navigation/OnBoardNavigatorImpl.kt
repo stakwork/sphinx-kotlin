d35c65235cc2b49f133906c2af45bd76c2fdb9c7
Matthew Nelson <developer@matthewnelson.io>
2021-03-06 06:26:29 -0500

    implement toDashboardScreen method

--- sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/navigation/OnBoardNavigatorImpl.kt
+++ sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/navigation/OnBoardNavigatorImpl.kt
@@ -1,12 +1,17 @@
 package chat.sphinx.activitymain.navigation
 
+import chat.sphinx.activitymain.R
+import chat.sphinx.dashboard.navigation.ToDashboardScreen
 import chat.sphinx.onboard.navigation.OnBoardNavigator
 import javax.inject.Inject
 
 class OnBoardNavigatorImpl @Inject constructor(
     navigationDriver: MainNavigationDriver
-): OnBoardNavigator(navigationDriver) {
+): OnBoardNavigator(navigationDriver)
+{
     override suspend fun toDashboardScreen() {
-        // TODO("Not yet implemented")
+        navigationDriver.submitNavigationRequest(
+            ToDashboardScreen(R.id.main_nav_graph)
+        )
     }
 }
