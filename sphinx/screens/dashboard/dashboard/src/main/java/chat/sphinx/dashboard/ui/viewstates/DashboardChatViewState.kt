e141253ce807dc7016b98d64879e66b2d7190f66
Matthew Nelson <developer@matthewnelson.io>
2021-03-09 09:14:35 -0500

    re-work view states for new motion layout implementation

--- sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/viewstates/DashboardChatViewState.kt
+++
@@ -1,7 +0,0 @@
-package chat.sphinx.dashboard.ui.viewstates
-
-import io.matthewnelson.concept_views.viewstate.ViewState
-
-internal sealed class DashboardChatViewState: ViewState<DashboardChatViewState>() {
-    object Idle: DashboardChatViewState()
-}
---
+++ sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/viewstates/NavDrawerViewState.kt
@@ -0,0 +1,39 @@
+package chat.sphinx.dashboard.ui.viewstates
+
+import androidx.constraintlayout.motion.widget.MotionLayout
+import chat.sphinx.dashboard.R
+import io.matthewnelson.android_concept_views.MotionLayoutViewState
+
+internal sealed class NavDrawerViewState: MotionLayoutViewState<NavDrawerViewState>() {
+
+    object Idle: NavDrawerViewState() {
+        override val startSetId: Int
+            get() = R.id.motion_scene_dashboard_drawer_closed
+        override val endSetId: Int
+            get() = R.id.motion_scene_dashboard_drawer_open
+
+        override fun restoreMotionScene(motionLayout: MotionLayout) {}
+        override fun transitionToEndSet(motionLayout: MotionLayout) {}
+    }
+
+    object Closed: NavDrawerViewState() {
+        override val startSetId: Int
+            get() = R.id.motion_scene_dashboard_drawer_open
+        override val endSetId: Int
+            get() = R.id.motion_scene_dashboard_drawer_closed
+
+        override fun restoreMotionScene(motionLayout: MotionLayout) {}
+    }
+
+    object Open: NavDrawerViewState() {
+        override val startSetId: Int
+            get() = R.id.motion_scene_dashboard_drawer_closed
+        override val endSetId: Int
+            get() = R.id.motion_scene_dashboard_drawer_open
+
+        override fun restoreMotionScene(motionLayout: MotionLayout) {
+            motionLayout.setTransition(R.id.transition_dashboard_drawer_closed_to_open)
+            motionLayout.setProgress(1F, 1F)
+        }
+    }
+}
