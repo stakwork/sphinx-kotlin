e258961c81741cf305828003c1f86e453dbd5513
Matthew Nelson <developer@matthewnelson.io>
2021-03-09 09:00:47 -0500

    lock screen orientation to portrait for all activities

--- sphinx/application/sphinx/src/main/java/chat/sphinx/authentication/SphinxAuthenticationCoreManager.kt
+++ sphinx/application/sphinx/src/main/java/chat/sphinx/authentication/SphinxAuthenticationCoreManager.kt
@@ -1,6 +1,9 @@
 package chat.sphinx.authentication
 
+import android.app.Activity
 import android.app.Application
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import io.matthewnelson.android_feature_authentication_core.components.AuthenticationCoreManagerAndroid
 import io.matthewnelson.android_feature_authentication_core.components.AuthenticationManagerInitializerAndroid
 import io.matthewnelson.concept_authentication.state.AuthenticationState
@@ -30,5 +33,10 @@ class SphinxAuthenticationCoreManager @Inject constructor(
 
     fun logOut() {
         updateAuthenticationState(AuthenticationState.Required.InitialLogIn)
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        super.onActivityCreated(activity, savedInstanceState)
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
