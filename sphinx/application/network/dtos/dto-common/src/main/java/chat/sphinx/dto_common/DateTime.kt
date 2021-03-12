31770d3d14910009f6227b764364d0b870045c76
Matthew Nelson <developer@matthewnelson.io>
2021-03-12 11:04:44 -0500

    add note for extension function creation for the DateTime wrapper

--- sphinx/application/network/dtos/dto-common/src/main/java/chat/sphinx/dto_common/DateTime.kt
+++ sphinx/application/network/dtos/dto-common/src/main/java/chat/sphinx/dto_common/DateTime.kt
@@ -1,5 +1,11 @@
 package chat.sphinx.dto_common
 
+// TODO: Write extension functions.
+//  Contact and Chat Dto fields 'created_at' and 'updated_at'
+//  are formatted like: 2021-02-26T10:48:20.025Z
+//  whereas the Contact's 'last_active' field's format
+//  is: 2021-03-12 14:26:18.000 +00:00
+
 inline class DateTime(val value: String) {
     init {
         require(value.isNotEmpty()) {
