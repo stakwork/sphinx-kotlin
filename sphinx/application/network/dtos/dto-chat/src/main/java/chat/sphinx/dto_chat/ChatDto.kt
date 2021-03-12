ab314d8eec60a45bae536f1fa142fa43eb683859
Matthew Nelson <developer@matthewnelson.io>
2021-03-11 19:26:27 -0500

    add inline function helpers for boolean values

--- sphinx/application/network/dtos/dto-chat/src/main/java/chat/sphinx/dto_chat/ChatDto.kt
+++ sphinx/application/network/dtos/dto-chat/src/main/java/chat/sphinx/dto_chat/ChatDto.kt
@@ -1,14 +1,35 @@
 package chat.sphinx.dto_chat
 
 import chat.sphinx.dto_chat.model.*
-import chat.sphinx.dto_common.DateTime
 import chat.sphinx.dto_chat.model.ChatGroupKey
-import chat.sphinx.dto_common.Deleted
-import chat.sphinx.dto_common.PhotoUrl
-import chat.sphinx.dto_common.Sats
+import chat.sphinx.dto_common.*
 import chat.sphinx.dto_common.chat.ChatId
 import chat.sphinx.dto_common.contact.ContactId
 import chat.sphinx.dto_common.contact.NodePubKey
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun ChatDto.isMuted(): Boolean =
+    isMuted.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun ChatDto.isDeleted(): Boolean =
+    deleted.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun ChatDto.isListed(): Boolean =
+    unlisted.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun ChatDto.isPrivate(): Boolean =
+    private.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun ChatDto.hasBeenSeen(): Boolean =
+    seen.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun ChatDto.hasPendingContacts(): Boolean =
+    pendingContactIds.isNotEmpty()
 
 class ChatDto(
     val id: ChatId,
