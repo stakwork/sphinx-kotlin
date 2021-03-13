3bab13250366285be147d151b0690e1b63f87e28
Matthew Nelson <developer@matthewnelson.io>
2021-03-13 10:59:07 -0500

    add dependency injection for NetworkModule

--- sphinx/application/sphinx/src/main/java/chat/sphinx/di/AppModule.kt
+++ sphinx/application/sphinx/src/main/java/chat/sphinx/di/AppModule.kt
@@ -1,16 +1,14 @@
 package chat.sphinx.di
 
-import chat.sphinx.concept_relay.RelayDataHandler
-import chat.sphinx.feature_relay.RelayDataHandlerImpl
+import chat.sphinx.BuildConfig
 import chat.sphinx.util.SphinxDispatchers
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import io.matthewnelson.concept_authentication.data.AuthenticationStorage
+import io.matthewnelson.build_config.BuildConfigDebug
+import io.matthewnelson.build_config.BuildConfigVersionCode
 import io.matthewnelson.concept_coroutines.CoroutineDispatchers
-import io.matthewnelson.concept_encryption_key.EncryptionKeyHandler
-import io.matthewnelson.feature_authentication_core.AuthenticationCoreManager
 import javax.inject.Singleton
 
 @Module
@@ -25,22 +23,11 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRelayDataHandlerImpl(
-        authenticationStorage: AuthenticationStorage,
-        authenticationCoreManager: AuthenticationCoreManager,
-        dispatchers: CoroutineDispatchers,
-        encryptionKeyHandler: EncryptionKeyHandler
-    ): RelayDataHandlerImpl =
-        RelayDataHandlerImpl(
-            authenticationStorage,
-            authenticationCoreManager,
-            dispatchers,
-            encryptionKeyHandler
-        )
+    fun provideBuildConfigDebug(): BuildConfigDebug =
+        BuildConfigDebug(BuildConfig.DEBUG)
 
     @Provides
-    fun provideRelayDataHandler(
-        relayDataHandlerImpl: RelayDataHandlerImpl
-    ): RelayDataHandler =
-        relayDataHandlerImpl
+    @Singleton
+    fun provideBuildConfigVersionCode(): BuildConfigVersionCode =
+        BuildConfigVersionCode(BuildConfig.VERSION_CODE)
 }
---
+++ sphinx/application/sphinx/src/main/java/chat/sphinx/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package chat.sphinx.di
+
+import chat.sphinx.concept_network_client.NetworkClient
+import chat.sphinx.concept_relay.RelayDataHandler
+import chat.sphinx.feature_network_client.NetworkClientImpl
+import chat.sphinx.feature_relay.RelayDataHandlerImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.matthewnelson.build_config.BuildConfigDebug
+import io.matthewnelson.concept_authentication.data.AuthenticationStorage
+import io.matthewnelson.concept_coroutines.CoroutineDispatchers
+import io.matthewnelson.concept_encryption_key.EncryptionKeyHandler
+import io.matthewnelson.feature_authentication_core.AuthenticationCoreManager
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRelayDataHandlerImpl(
+        authenticationStorage: AuthenticationStorage,
+        authenticationCoreManager: AuthenticationCoreManager,
+        dispatchers: CoroutineDispatchers,
+        encryptionKeyHandler: EncryptionKeyHandler
+    ): RelayDataHandlerImpl =
+        RelayDataHandlerImpl(
+            authenticationStorage,
+            authenticationCoreManager,
+            dispatchers,
+            encryptionKeyHandler
+        )
+
+    @Provides
+    fun provideRelayDataHandler(
+        relayDataHandlerImpl: RelayDataHandlerImpl
+    ): RelayDataHandler =
+        relayDataHandlerImpl
+
+    @Provides
+    @Singleton
+    fun provideNetworkClientImpl(
+        buildConfigDebug: BuildConfigDebug
+    ): NetworkClientImpl =
+        NetworkClientImpl(buildConfigDebug)
+
+    @Provides
+    fun provideNetworkClient(
+        networkClientImpl: NetworkClientImpl
+    ): NetworkClient =
+        networkClientImpl
+}
