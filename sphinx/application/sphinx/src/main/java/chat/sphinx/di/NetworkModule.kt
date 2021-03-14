4e7966e0ef71cdf47f83d67dac900c763bd21ee4
Matthew Nelson <developer@matthewnelson.io>
2021-03-13 21:43:57 -0500

    add dependency injection for NetworkQueryChat

--- sphinx/application/sphinx/src/main/java/chat/sphinx/di/NetworkModule.kt
+++ sphinx/application/sphinx/src/main/java/chat/sphinx/di/NetworkModule.kt
@@ -1,9 +1,12 @@
 package chat.sphinx.di
 
 import chat.sphinx.concept_network_client.NetworkClient
+import chat.sphinx.concept_network_query_chat.NetworkQueryChat
 import chat.sphinx.concept_relay.RelayDataHandler
 import chat.sphinx.feature_network_client.NetworkClientImpl
+import chat.sphinx.feature_network_query_chat.NetworkQueryChatImpl
 import chat.sphinx.feature_relay.RelayDataHandlerImpl
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -52,4 +55,30 @@ object NetworkModule {
         networkClientImpl: NetworkClientImpl
     ): NetworkClient =
         networkClientImpl
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideNetworkQueryChatImpl(
+        dispatchers: CoroutineDispatchers,
+        moshi: Moshi,
+        networkClient: NetworkClient,
+        relayDataHandler: RelayDataHandler
+    ): NetworkQueryChatImpl =
+        NetworkQueryChatImpl(
+            dispatchers,
+            moshi,
+            networkClient,
+            relayDataHandler
+        )
+
+    @Provides
+    fun provideNetworkQueryChat(
+        networkQueryChatImpl: NetworkQueryChatImpl
+    ): NetworkQueryChat =
+        networkQueryChatImpl
 }
