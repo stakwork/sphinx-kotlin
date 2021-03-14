21b7663305b47a6432a38801defcf0f98ab5d2fc
Matthew Nelson <developer@matthewnelson.io>
2021-03-14 08:37:45 -0400

    initialze modules for invite queries

---
+++ sphinx/application/network/concepts/queries/concept-network-query-invite/src/main/java/chat/sphinx/concept_network_query_invite/NetworkQueryInvite.kt
@@ -0,0 +1,23 @@
+package chat.sphinx.concept_network_query_invite
+
+abstract class NetworkQueryInvite {
+
+    ///////////
+    /// GET ///
+    ///////////
+
+    ///////////
+    /// PUT ///
+    ///////////
+
+    ////////////
+    /// POST ///
+    ////////////
+//    app.post('/invites', invites.createInvite)
+//    app.post('/invites/:invite_string/pay', invites.payInvite)
+//    app.post('/invites/finish', invites.finishInvite)
+
+    //////////////
+    /// DELETE ///
+    //////////////
+}
