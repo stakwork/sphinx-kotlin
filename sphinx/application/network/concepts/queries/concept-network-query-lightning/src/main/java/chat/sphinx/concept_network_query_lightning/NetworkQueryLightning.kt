1081a1b298921c4a79f093b55167fb044bbe82d5
Matthew Nelson <developer@matthewnelson.io>
2021-03-15 14:11:54 -0400

    initialize network query lightning modules

---
+++ sphinx/application/network/concepts/queries/concept-network-query-lightning/src/main/java/chat/sphinx/concept_network_query_lightning/NetworkQueryLightning.kt
@@ -0,0 +1,24 @@
+package chat.sphinx.concept_network_query_lightning
+
+abstract class NetworkQueryLightning {
+
+    ///////////
+    /// GET ///
+    ///////////
+//    app.get('/invoices', invoices.listInvoices)
+
+    ///////////
+    /// PUT ///
+    ///////////
+//    app.put('/invoices', invoices.payInvoice)
+
+    ////////////
+    /// POST ///
+    ////////////
+//    app.post('/invoices', invoices.createInvoice)
+//    app.post('/invoices/cancel', invoices.cancelInvoice)
+
+    //////////////
+    /// DELETE ///
+    //////////////
+}
---
+++ sphinx/application/network/features/queries/feature-network-query-lightning/src/main/java/chat/sphinx/feature_network_query_lightning/NetworkQueryLightningImpl.kt
@@ -0,0 +1,4 @@
+package chat.sphinx.feature_network_query_lightning
+
+class NetworkQueryLightningImpl {
+}
