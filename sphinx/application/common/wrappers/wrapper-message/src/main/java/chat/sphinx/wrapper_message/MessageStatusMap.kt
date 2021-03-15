492b9522f00019fe9b85212af1bfa3a937c63b6f
Matthew Nelson <developer@matthewnelson.io>
2021-03-15 13:36:30 -0400

    update the MessageDto's status_map parameter to a <Long, Int?> type and add transformation function for wrapper classes

--- sphinx/application/common/wrappers/wrapper-message/src/main/java/chat/sphinx/wrapper_message/MessageStatusMap.kt
+++ sphinx/application/common/wrappers/wrapper-message/src/main/java/chat/sphinx/wrapper_message/MessageStatusMap.kt
@@ -2,7 +2,11 @@ package chat.sphinx.wrapper_message
 
 import chat.sphinx.wrapper_common.contact.ContactId
 
-class MessageStatusMap(
-    val contactId: ContactId,
-    val status: MessageStatus
-)
+@Suppress("NOTHING_TO_INLINE")
+@Throws(IllegalArgumentException::class)
+inline fun Map<Long, Int?>.toMessageStatusMap(): Map<ContactId, MessageStatus> =
+    this.mapKeys { ContactId(it.key) }.mapValues { it.value.toMessageStatus() }
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Map<ContactId, MessageStatus>.toPrimitivesMap(): Map<Long, Int?> =
+    mapKeys { it.key.value }.mapValues { it.value.value }
