7c23a488dd7336fb90b2c7ff69732ee9564e4c01
Matthew Nelson <developer@matthewnelson.io>
2021-03-24 14:04:01 -0400

    add generic class mapper abstraction

---
+++ sphinx/application/data/features/feature-coredb/src/main/java/chat/sphinx/feature_coredb/mappers/ClassMapper.kt
@@ -0,0 +1,15 @@
+package chat.sphinx.feature_coredb.mappers
+
+internal abstract class ClassMapper<DTO, DBO, Presenter> {
+
+    abstract fun fromDTOtoDBO(dto: DTO): DBO
+    abstract fun fromDTOtoPresenter(dto: DTO): Presenter
+    abstract fun fromDTOsToDBOs(dtos: List<DTO>): List<DBO>
+    abstract fun fromDTOsToPresenters(dtos: List<DTO>): List<Presenter>
+
+    abstract fun fromDBOtoPresenter(dbo: DBO): Presenter
+    abstract fun fromDBOsToPresenters(dbos: List<DBO>): List<Presenter>
+
+    abstract fun fromPresenterToDBO(presenter: Presenter): DBO
+    abstract fun fromPresentersToDBOs(presenters: List<Presenter>): List<DBO>
+}
