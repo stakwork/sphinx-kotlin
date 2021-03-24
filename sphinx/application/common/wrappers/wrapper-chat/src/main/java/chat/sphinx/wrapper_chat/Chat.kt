269bd0bdfe6c6e555081879047b648373a709bda
Matthew Nelson <developer@matthewnelson.io>
2021-03-24 13:58:27 -0400

    implement a Chat presenter object

---
+++ sphinx/application/common/wrappers/wrapper-chat/src/main/java/chat/sphinx/wrapper_chat/Chat.kt
@@ -0,0 +1,54 @@
+package chat.sphinx.wrapper_chat
+
+import chat.sphinx.wrapper_common.DateTime
+import chat.sphinx.wrapper_common.PhotoUrl
+import chat.sphinx.wrapper_common.Seen
+import chat.sphinx.wrapper_common.chat.ChatId
+import chat.sphinx.wrapper_common.contact.ContactId
+import chat.sphinx.wrapper_common.isTrue
+import chat.sphinx.wrapper_common.lightning.LightningNodePubKey
+import chat.sphinx.wrapper_common.lightning.Sat
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Chat.isMuted(): Boolean =
+    isMuted.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Chat.isUnlisted(): Boolean =
+    unlisted.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Chat.isPrivateTribe(): Boolean =
+    privateTribe.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Chat.hasBeenSeen(): Boolean =
+    seen.isTrue()
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Chat.hasPendingContacts(): Boolean =
+    !pendingContactIds.isNullOrEmpty()
+
+data class Chat(
+    val id: ChatId,
+    val uuid: ChatUUID,
+    val name: ChatName?,
+    val photoUrl: PhotoUrl?,
+    val type: ChatType,
+    val status: ChatStatus,
+    val contactIds: List<ContactId>,
+    val isMuted: ChatMuted,
+    val createdAt: DateTime,
+    val groupKey: ChatGroupKey?,
+    val host: ChatHost?,
+    val pricePerMessage: Sat?,
+    val escrowAmount: Sat?,
+    val unlisted: ChatUnlisted,
+    val privateTribe: ChatPrivate,
+    val ownerPubKey: LightningNodePubKey?,
+    val seen: Seen,
+    val metaData: ChatMetaData?,
+    val myPhotoUrl: PhotoUrl?,
+    val myAlias: ChatAlias?,
+    val pendingContactIds: List<ContactId>?,
+)
