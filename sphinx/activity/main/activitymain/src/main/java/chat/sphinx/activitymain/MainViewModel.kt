db01ad4590f0fa87c43e8ec85d1692366cb5a83a
Matthew Nelson <developer@matthewnelson.io>
2021-03-26 06:23:43 -0400

    add automatic DB initialization upon login

--- sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/MainViewModel.kt
+++ sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/MainViewModel.kt
@@ -1,20 +1,24 @@
 package chat.sphinx.activitymain
 
 import androidx.lifecycle.viewModelScope
+import app.cash.exhaustive.Exhaustive
 import chat.sphinx.activitymain.navigation.drivers.AuthenticationNavigationDriver
 import chat.sphinx.activitymain.navigation.drivers.DetailNavigationDriver
 import chat.sphinx.activitymain.navigation.drivers.PrimaryNavigationDriver
 import chat.sphinx.activitymain.ui.MainViewState
+import chat.sphinx.feature_coredb.SphinxCoreDBImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.matthewnelson.android_feature_activity.NavigationViewModel
 import io.matthewnelson.android_feature_viewmodel.BaseViewModel
 import io.matthewnelson.concept_authentication.coordinator.AuthenticationCoordinator
 import io.matthewnelson.concept_authentication.coordinator.AuthenticationRequest
+import io.matthewnelson.concept_authentication.coordinator.AuthenticationResponse
 import io.matthewnelson.concept_authentication.state.AuthenticationState
 import io.matthewnelson.concept_authentication.state.AuthenticationStateManager
 import io.matthewnelson.concept_coroutines.CoroutineDispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,17 +28,48 @@ internal class MainViewModel @Inject constructor(
     val authenticationDriver: AuthenticationNavigationDriver,
     val detailDriver: DetailNavigationDriver,
     private val dispatchers: CoroutineDispatchers,
-    override val navigationDriver: PrimaryNavigationDriver
+    override val navigationDriver: PrimaryNavigationDriver,
+    private val sphinxCoreDB: SphinxCoreDBImpl,
 ): BaseViewModel<MainViewState>(MainViewState.DetailScreenInactive), NavigationViewModel<PrimaryNavigationDriver>
 {
     init {
         viewModelScope.launch(dispatchers.mainImmediate) {
             authenticationStateManager.authenticationStateFlow.collect { state ->
-                if (state is AuthenticationState.Required.LoggedOut) {
-                    authenticationCoordinator.submitAuthenticationRequest(
-                        AuthenticationRequest.LogIn(privateKey = null)
-                    )
+                @Exhaustive
+                when (state) {
+                    is AuthenticationState.NotRequired -> {
+                        initializeCoreDB()
+                    }
+                    is AuthenticationState.Required.InitialLogIn -> {}
+                    is AuthenticationState.Required.LoggedOut -> {
+                        authenticationCoordinator.submitAuthenticationRequest(
+                            AuthenticationRequest.LogIn(privateKey = null)
+                        )
+                    }
                 }
+            }
+        }
+    }
+
+    private var initializeCoreDBJob: Job? = null
+    private fun initializeCoreDB() {
+        if (sphinxCoreDB.isInitialized || initializeCoreDBJob?.isActive == true) {
+            return
+        }
+
+        initializeCoreDBJob = viewModelScope.launch(dispatchers.mainImmediate) {
+            val request = AuthenticationRequest.GetEncryptionKey()
+            while (
+                !sphinxCoreDB.isInitialized &&
+                currentCoroutineContext().isActive
+            ) {
+                authenticationCoordinator.submitAuthenticationRequest(request)
+                    .firstOrNull().let { response ->
+                        if (response is AuthenticationResponse.Success.Key) {
+                            sphinxCoreDB.initializeDatabase(response.encryptionKey)
+                            delay(15L)
+                        }
+                    }
             }
         }
     }
