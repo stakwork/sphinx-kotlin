f2ac709f5c46001983a5532a607df9f80670aae7
Matthew Nelson <developer@matthewnelson.io>
2021-03-27 09:47:21 -0400

    add wrapper classes for media

---
+++ sphinx/application/common/wrappers/wrapper-media/src/main/java/chat/sphinx/wrapper_media/MediaKey.kt
@@ -0,0 +1,17 @@
+package chat.sphinx.wrapper_media
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun String.toMediaKey(): MediaKey? =
+    try {
+        MediaKey(this)
+    } catch (e: IllegalArgumentException) {
+        null
+    }
+
+inline class MediaKey(val value: String) {
+    init {
+        require(value.isNotEmpty()) {
+            "MediaKey cannot be empty"
+        }
+    }
+}
---
+++ sphinx/application/common/wrappers/wrapper-media/src/main/java/chat/sphinx/wrapper_media/MediaType.kt
@@ -0,0 +1,19 @@
+package chat.sphinx.wrapper_media
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun String.toMediaType(): MediaType? =
+    try {
+        MediaType(this)
+    } catch (e: IllegalArgumentException) {
+        null
+    }
+
+// TODO: Build out extensions for different types
+
+inline class MediaType(val value: String) {
+    init {
+        require(value.isNotEmpty()) {
+            "MediaType cannot be empty"
+        }
+    }
+}
