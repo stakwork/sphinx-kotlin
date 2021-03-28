32fa5fe4b6e02fcf36aedc1fea986ac844c2b20e
Matthew Nelson <developer@matthewnelson.io>
2021-03-28 10:03:48 -0400

    add inline functions for formatting pub/priv keys to certificates

--- sphinx/application/data/concepts/concept-crypto/src/main/java/chat/sphinx/concept_crypto/rsa/RSAKeyPair.kt
+++ sphinx/application/data/concepts/concept-crypto/src/main/java/chat/sphinx/concept_crypto/rsa/RSAKeyPair.kt
@@ -3,6 +3,40 @@ package chat.sphinx.concept_crypto.rsa
 inline class RsaPrivateKey(val value: CharArray)
 inline class RsaPublicKey(val value: CharArray)
 
+@Suppress("NOTHING_TO_INLINE")
+inline fun RsaPrivateKey.formatToCert(singleLine: Boolean = false): String {
+    val sb = StringBuilder()
+
+    sb.append("-----BEGIN RSA PRIVATE KEY-----\n")
+
+    if (!singleLine) {
+        sb.append(value.joinToString("").replace("(.{64})".toRegex(), "$1\n"))
+    } else {
+        sb.append(value.joinToString(""))
+    }
+
+    sb.append("\n-----END RSA PRIVATE KEY-----")
+
+    return sb.toString()
+}
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun RsaPublicKey.formatToCert(singleLine: Boolean = false): String {
+    val sb = StringBuilder()
+
+    sb.append("-----BEGIN RSA PUBLIC KEY-----\n")
+
+    if (!singleLine) {
+        sb.append(value.joinToString("").replace("(.{64})".toRegex(), "$1\n"))
+    } else {
+        sb.append(value.joinToString(""))
+    }
+
+    sb.append("\n-----END RSA PUBLIC KEY-----")
+
+    return sb.toString()
+}
+
 class RSAKeyPair(
     val privateKey: RsaPrivateKey,
     val publicKey: RsaPublicKey,
