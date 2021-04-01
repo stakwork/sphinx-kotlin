c80c9419d51364c99af414511f94dca413c07527
Matthew Nelson <developer@matthewnelson.io>
2021-04-01 10:02:46 -0400

    remove skip_broadcast_joins field

--- sphinx/application/network/concepts/queries/concept-network-query-chat/src/main/java/chat/sphinx/concept_network_query_chat/model/BaseChatDto.kt
+++ sphinx/application/network/concepts/queries/concept-network-query-chat/src/main/java/chat/sphinx/concept_network_query_chat/model/BaseChatDto.kt
@@ -43,6 +43,5 @@ abstract class BaseChatDto<IsMuted, Unlisted, Private, Seen> {
     abstract val meta: String?
     abstract val my_photo_url: String?
     abstract val my_alias: String?
-    abstract val skip_broadcast_joins: Int?
     abstract val pending_contact_ids: List<Long>?
 }
