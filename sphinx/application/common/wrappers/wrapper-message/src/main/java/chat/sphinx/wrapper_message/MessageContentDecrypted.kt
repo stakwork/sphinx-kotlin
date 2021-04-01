90666e27debf1c85aff55467b65d2d4945945634
Matthew Nelson <developer@matthewnelson.io>
2021-04-01 10:10:24 -0400

    create inline function for converting a String value to MessageContentDecrypted or null

--- sphinx/application/common/wrappers/wrapper-message/src/main/java/chat/sphinx/wrapper_message/MessageContentDecrypted.kt
+++ sphinx/application/common/wrappers/wrapper-message/src/main/java/chat/sphinx/wrapper_message/MessageContentDecrypted.kt
@@ -1,5 +1,13 @@
 package chat.sphinx.wrapper_message
 
+@Suppress("NOTHING_TO_INLINE")
+inline fun String.toMessageContentDecrypted(): MessageContentDecrypted? =
+    try {
+        MessageContentDecrypted(this)
+    } catch (e: IllegalArgumentException) {
+        null
+    }
+
 inline class MessageContentDecrypted(val value: String) {
     init {
         require(value.isNotEmpty()) {
