98051cdb25c6de1546980214d731803cdf3dbefe
Matthew Nelson <developer@matthewnelson.io>
2021-04-01 12:42:04 -0400

    add debug/release version checking

--- sphinx/application/sphinx/src/main/java/chat/sphinx/util/SphinxLoggerImpl.kt
+++ sphinx/application/sphinx/src/main/java/chat/sphinx/util/SphinxLoggerImpl.kt
@@ -4,20 +4,27 @@ import android.util.Log
 import app.cash.exhaustive.Exhaustive
 import chat.sphinx.logger.LogType
 import chat.sphinx.logger.SphinxLogger
+import io.matthewnelson.build_config.BuildConfigDebug
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class SphinxLoggerImpl @Inject constructor(): SphinxLogger() {
+class SphinxLoggerImpl @Inject constructor(
+    val buildConfigDebug: BuildConfigDebug,
+): SphinxLogger() {
     override fun log(tag: String, message: String, type: LogType) {
 
         @Exhaustive
         when (type) {
             LogType.Debug -> {
-                Log.d(tag, message)
+                if (buildConfigDebug.value) {
+                    Log.d(tag, message)
+                }
             }
             LogType.Info -> {
-                Log.i(tag, message)
+                if (buildConfigDebug.value) {
+                    Log.i(tag, message)
+                }
             }
             LogType.Warning -> {
                 Log.w(tag, message)
