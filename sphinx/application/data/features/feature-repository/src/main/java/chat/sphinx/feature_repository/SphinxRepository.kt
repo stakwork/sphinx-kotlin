55b449c69d85bbdf9bf0970059ca2a40d488ba60
Matthew Nelson <developer@matthewnelson.io>
2021-04-01 10:20:36 -0400

    implement networkRefreshMessages API

--- sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/SphinxRepository.kt
+++ sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/SphinxRepository.kt
@@ -3,7 +3,9 @@ package chat.sphinx.feature_repository
 import app.cash.exhaustive.Exhaustive
 import chat.sphinx.concept_coredb.CoreDB
 import chat.sphinx.concept_coredb.util.upsertChat
+import chat.sphinx.concept_coredb.util.upsertMessage
 import chat.sphinx.concept_crypto_rsa.RSA
+import chat.sphinx.concept_crypto_rsa.RsaPrivateKey
 import chat.sphinx.concept_repository_chat.ChatRepository
 import chat.sphinx.concept_repository_message.MessageRepository
 import chat.sphinx.concept_network_query_chat.NetworkQueryChat
@@ -19,12 +21,21 @@ import chat.sphinx.feature_repository.mappers.message.MessageDtoDboMapper
 import chat.sphinx.wrapper_chat.Chat
 import chat.sphinx.wrapper_common.chat.ChatUUID
 import chat.sphinx.wrapper_common.chat.ChatId
+import chat.sphinx.wrapper_common.message.MessageId
+import chat.sphinx.wrapper_common.message.MessagePagination
 import chat.sphinx.wrapper_message.Message
+import chat.sphinx.wrapper_message.MessageContent
+import chat.sphinx.wrapper_message.MessageType
 import com.squareup.sqldelight.runtime.coroutines.asFlow
 import com.squareup.sqldelight.runtime.coroutines.mapToList
 import com.squareup.sqldelight.runtime.coroutines.mapToOneOrNull
 import io.matthewnelson.concept_coroutines.CoroutineDispatchers
 import io.matthewnelson.feature_authentication_core.AuthenticationCoreManager
+import io.matthewnelson.k_openssl_common.annotations.RawPasswordAccess
+import io.matthewnelson.k_openssl_common.annotations.UnencryptedDataAccess
+import io.matthewnelson.k_openssl_common.clazzes.EncryptedString
+import io.matthewnelson.k_openssl_common.clazzes.UnencryptedByteArray
+import io.matthewnelson.k_openssl_common.clazzes.toUnencryptedString
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.sync.Mutex
@@ -160,6 +171,18 @@ class SphinxRepository(
         MessageDboPresenterMapper(dispatchers)
     }
 
+    @OptIn(RawPasswordAccess::class)
+    private suspend fun decryptMessageContent(
+        messageContent: MessageContent
+    ): Response<UnencryptedByteArray, ResponseError> =
+        authenticationCoreManager.getEncryptionKey()?.let { keys ->
+            rsa.decrypt(
+                rsaPrivateKey = RsaPrivateKey(keys.privateKey.value),
+                text = EncryptedString(messageContent.value),
+                dispatcher = dispatchers.default
+            )
+        } ?: Response.Error(ResponseError("EncryptionKey retrieval failed"))
+
     override suspend fun getLatestMessageForChat(chatId: ChatId): Flow<Message?> {
         TODO("Not yet implemented")
     }
@@ -192,7 +215,131 @@ class SphinxRepository(
         TODO("Not yet implemented")
     }
 
-    override fun networkRefreshMessages(): Flow<LoadResponse<Boolean, ResponseError>> {
-        TODO("Not yet implemented")
+    @OptIn(UnencryptedDataAccess::class)
+    override fun networkRefreshMessages(): Flow<LoadResponse<Boolean, ResponseError>> = flow {
+        emit(LoadResponse.Loading)
+        val queries = coreDB.getSphinxDatabaseQueries()
+        var lastMessageId: Long = withContext(dispatchers.io) {
+            queries.getLatestMessageId().executeAsOneOrNull()?.value ?: 0
+        }
+
+        val supervisor = SupervisorJob(currentCoroutineContext().job)
+        val scope = CoroutineScope(supervisor)
+
+        var error: Response.Error<ResponseError>? = null
+
+        while (currentCoroutineContext().isActive && lastMessageId >= 0) {
+
+            networkQueryMessage.getMessages(
+                MessagePagination.instantiate(200, lastMessageId.toInt(), null)
+            ).collect { response ->
+                @Exhaustive
+                when (response) {
+                    is LoadResponse.Loading -> {}
+                    is Response.Error -> {
+                        lastMessageId = -1
+                        error = response
+                    }
+                    is Response.Success -> {
+                        val newMessages = response.value.new_messages
+
+                        if (newMessages.isNotEmpty()) {
+                            val decryptMap: MutableMap<MessageId, String> = mutableMapOf()
+
+                            for (message in newMessages) {
+                                message.message_content?.let { content ->
+                                    if (content.isNotEmpty() && message.type != MessageType.KEY_SEND) {
+                                        decryptMap[MessageId(message.id)] = content
+                                    }
+                                }
+                            }
+
+                            // TODO: Move out to separate class to encapsulate
+                            val decryptMapCounterLock = Mutex()
+                            var counter = decryptMap.size
+
+                            for (key in decryptMap.keys) {
+                                scope.launch decrypt@ {
+                                    val content: String = decryptMapCounterLock.withLock {
+                                        decryptMap[key] ?: let {
+                                            counter--
+                                            return@decrypt
+                                        }
+                                    }
+
+                                    val decrypted = decryptMessageContent(MessageContent(content))
+
+                                    decryptMapCounterLock.withLock {
+                                        @Exhaustive
+                                        when (decrypted) {
+                                            is Response.Error -> {
+                                                decryptMap.remove(key)
+                                                counter--
+                                            }
+                                            is Response.Success -> {
+                                                decryptMap[key] =
+                                                    decrypted.value.toUnencryptedString().value
+                                                counter--
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            while (currentCoroutineContext().isActive) {
+                                delay(100L)
+                                if (decryptMapCounterLock.withLock { counter } <= 1) {
+                                    break
+                                }
+                            }
+
+                            for (message in newMessages) {
+                                decryptMap[MessageId(message.id)]?.let { decrypted ->
+                                    message.setMessageContentDecrypted(decrypted)
+                                }
+                            }
+
+                            val dbos = messageDtoDboMapper.mapListFrom(newMessages)
+
+                            chatLock.withLock {
+                                messageLock.withLock {
+                                    withContext(dispatchers.io) {
+
+                                        queries.transaction {
+                                            val chatIds = queries.getAllChatIds().executeAsList()
+                                            for (dbo in dbos) {
+                                                if (dbo.chat_id.value == MessageDtoDboMapper.NULL_CHAT_ID.toLong()) {
+                                                    queries.upsertMessage(dbo)
+                                                } else if (chatIds.contains(dbo.chat_id)) {
+                                                    queries.upsertMessage(dbo)
+                                                }
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+
+                        }
+
+                        when {
+                            lastMessageId == -1 -> {}
+                            newMessages.size >= 199 -> {
+                                lastMessageId += 200
+                            }
+                            else -> {
+                                lastMessageId = -1
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        supervisor.cancelAndJoin()
+
+        error?.let { responseError ->
+            emit(responseError)
+        } ?: emit(Response.Success(true))
     }
 }
