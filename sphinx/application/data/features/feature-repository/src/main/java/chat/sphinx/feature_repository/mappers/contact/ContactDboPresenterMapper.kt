2fbfd061db983791ef05a37dd3b50ae07985f108
Matthew Nelson <developer@matthewnelson.io>
2021-04-02 14:40:20 -0400

    build out contact mappers

--- sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/mappers/contact/ContactDboPresenterMapper.kt
+++ sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/mappers/contact/ContactDboPresenterMapper.kt
@@ -3,16 +3,55 @@ package chat.sphinx.feature_repository.mappers.contact
 import chat.sphinx.conceptcoredb.ContactDbo
 import chat.sphinx.feature_repository.mappers.ClassMapper
 import chat.sphinx.wrapper_contact.Contact
+import chat.sphinx.wrapper_contact.ContactFromGroup
 import io.matthewnelson.concept_coroutines.CoroutineDispatchers
 
 internal class ContactDboPresenterMapper(
     dispatchers: CoroutineDispatchers
 ): ClassMapper<ContactDbo, Contact>(dispatchers) {
     override suspend fun mapFrom(value: ContactDbo): Contact {
-        TODO("Not yet implemented")
+        return Contact(
+            value.id,
+            value.route_hint,
+            value.node_pub_key,
+            value.node_alias,
+            value.alias,
+            value.photo_url,
+            value.private_photo,
+            value.owner,
+            value.status,
+            value.public_key,
+            value.device_id,
+            value.created_at,
+            value.updated_at,
+
+            // Contacts from tribes are not stored in the DB
+            ContactFromGroup.False,
+
+            value.notification_sound,
+            value.tip_amount,
+            value.invite_id
+        )
     }
 
     override suspend fun mapTo(value: Contact): ContactDbo {
-        TODO("Not yet implemented")
+        return ContactDbo(
+            value.id,
+            value.routeHint,
+            value.nodePubKey,
+            value.nodeAlias,
+            value.alias,
+            value.photoUrl,
+            value.privatePhoto,
+            value.isOwner,
+            value.status,
+            value.rsaPublicKey,
+            value.deviceId,
+            value.createdAt,
+            value.updatedAt,
+            value.notificationSound,
+            value.tipAmount,
+            value.inviteId
+        )
     }
 }
