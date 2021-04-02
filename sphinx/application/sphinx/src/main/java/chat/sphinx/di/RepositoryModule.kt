091bad97e01e9afff77a09b11cc4bf600de9986a
Matthew Nelson <developer@matthewnelson.io>
2021-04-02 15:18:19 -0400

    implement dependency injection for new ContactRepository and needed constructor arguments for its implementation

--- sphinx/application/sphinx/src/main/java/chat/sphinx/di/RepositoryModule.kt
+++ sphinx/application/sphinx/src/main/java/chat/sphinx/di/RepositoryModule.kt
@@ -4,7 +4,9 @@ import chat.sphinx.concept_crypto_rsa.RSA
 import chat.sphinx.concept_repository_chat.ChatRepository
 import chat.sphinx.concept_repository_message.MessageRepository
 import chat.sphinx.concept_network_query_chat.NetworkQueryChat
+import chat.sphinx.concept_network_query_contact.NetworkQueryContact
 import chat.sphinx.concept_network_query_message.NetworkQueryMessage
+import chat.sphinx.concept_repository_contact.ContactRepository
 import chat.sphinx.database.SphinxCoreDBImpl
 import chat.sphinx.feature_coredb.CoreDBImpl
 import chat.sphinx.feature_repository.SphinxRepository
@@ -34,6 +36,7 @@ object RepositoryModule {
         coreDBImpl: CoreDBImpl,
         dispatchers: CoroutineDispatchers,
         networkQueryChat: NetworkQueryChat,
+        networkQueryContact: NetworkQueryContact,
         networkQueryMessage: NetworkQueryMessage,
         rsa: RSA,
         sphinxLogger: SphinxLogger
@@ -43,6 +46,7 @@ object RepositoryModule {
             coreDBImpl,
             dispatchers,
             networkQueryChat,
+            networkQueryContact,
             networkQueryMessage,
             rsa,
             sphinxLogger
@@ -52,6 +56,12 @@ object RepositoryModule {
     fun provideChatRepository(
         sphinxRepository: SphinxRepository
     ): ChatRepository =
+        sphinxRepository
+
+    @Provides
+    fun provideContactRepository(
+        sphinxRepository: SphinxRepository
+    ): ContactRepository =
         sphinxRepository
 
     @Provides
