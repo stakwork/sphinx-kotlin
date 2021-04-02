23c41d8b7e1b2c5e19d3173ae69e3e8bc049d741
Matthew Nelson <developer@matthewnelson.io>
2021-04-02 10:12:27 -0400

    initialize contact repository module

---
+++ sphinx/application/data/concepts/repositories/concept-repository-contact/src/main/java/chat/sphinx/concept_repository_contact/ContactRepository.kt
@@ -0,0 +1,17 @@
+package chat.sphinx.concept_repository_contact
+
+import chat.sphinx.kotlin_response.LoadResponse
+import chat.sphinx.kotlin_response.ResponseError
+import chat.sphinx.wrapper_common.contact.ContactId
+import chat.sphinx.wrapper_contact.Contact
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * All [Contact]s are cached to the DB such that a network refresh will update
+ * them, and thus proc and [Flow] being collected.
+ * */
+interface ContactRepository {
+    suspend fun getContacts(): Flow<List<Contact>>
+    suspend fun getContactById(contactId: ContactId): Flow<Contact?>
+    fun networkRefreshContacts(): Flow<LoadResponse<Boolean, ResponseError>>
+}
