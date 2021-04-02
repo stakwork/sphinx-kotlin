a6e331e4a211dee1419036870c6c21a32e1b2467
Matthew Nelson <developer@matthewnelson.io>
2021-04-02 12:34:33 -0400

    add nullability to contact alias and remove field 'deleted'

--- sphinx/application/common/wrappers/wrapper-contact/src/main/java/chat/sphinx/wrapper_contact/Contact.kt
+++ sphinx/application/common/wrappers/wrapper-contact/src/main/java/chat/sphinx/wrapper_contact/Contact.kt
@@ -1,7 +1,6 @@
 package chat.sphinx.wrapper_contact
 
 import chat.sphinx.wrapper_common.DateTime
-import chat.sphinx.wrapper_common.Deleted
 import chat.sphinx.wrapper_common.PhotoUrl
 import chat.sphinx.wrapper_common.contact.ContactId
 import chat.sphinx.wrapper_common.invite.InviteId
@@ -16,11 +15,10 @@ data class Contact(
     val routeHint: LightningRouteHint?,
     val nodePubKey: LightningNodePubKey?,
     val nodeAlias: LightningNodeAlias?,
-    val alias: ContactAlias,
+    val alias: ContactAlias?,
     val photoUrl: PhotoUrl?,
     val privatePhoto: PrivatePhoto,
     val isOwner: Owner,
-    val deleted: Deleted,
     val status: ContactStatus,
     val rsaPublicKey: RsaPublicKey?,
     val deviceId: DeviceId?,
--- sphinx/application/common/wrappers/wrapper-contact/src/main/java/chat/sphinx/wrapper_contact/ContactAlias.kt
+++ sphinx/application/common/wrappers/wrapper-contact/src/main/java/chat/sphinx/wrapper_contact/ContactAlias.kt
@@ -1,5 +1,13 @@
 package chat.sphinx.wrapper_contact
 
+@Suppress("NOTHING_TO_INLINE")
+inline fun String.toContactAlias(): ContactAlias? =
+    try {
+        ContactAlias(this)
+    } catch (e: IllegalArgumentException) {
+        null
+    }
+
 inline class ContactAlias(val value: String) {
     init {
         require(value.isNotEmpty()) {
