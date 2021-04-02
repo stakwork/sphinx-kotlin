1a384065d2fbe2941c0202a54bef8e60551ce833
Matthew Nelson <developer@matthewnelson.io>
2021-04-01 22:57:45 -0400

    change dashboard navigator argument to type ChatId

--- sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/navigation/navigators/primary/DashboardNavigatorImpl.kt
+++ sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/navigation/navigators/primary/DashboardNavigatorImpl.kt
@@ -5,21 +5,22 @@ import chat.sphinx.chat_contact.navigation.ToChatContactScreen
 import chat.sphinx.chat_group.navigation.ToChatGroupScreen
 import chat.sphinx.chat_tribe.navigation.ToChatTribeScreen
 import chat.sphinx.dashboard.navigation.DashboardNavigator
+import chat.sphinx.wrapper_common.chat.ChatId
 import javax.inject.Inject
 
 class DashboardNavigatorImpl @Inject constructor(
     navigationDriver: PrimaryNavigationDriver
 ): DashboardNavigator(navigationDriver)
 {
-    override suspend fun toChatContact(chatId: String) {
+    override suspend fun toChatContact(chatId: ChatId) {
         navigationDriver.submitNavigationRequest(ToChatContactScreen(/*chatId*/))
     }
 
-    override suspend fun toChatGroup(chatId: String) {
+    override suspend fun toChatGroup(chatId: ChatId) {
         navigationDriver.submitNavigationRequest(ToChatGroupScreen(/*chatId*/))
     }
 
-    override suspend fun toChatTribe(chatId: String) {
+    override suspend fun toChatTribe(chatId: ChatId) {
         navigationDriver.submitNavigationRequest(ToChatTribeScreen(/*chatId*/))
     }
 }
