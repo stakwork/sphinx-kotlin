a6f88a10c169ce2796810ac21ee44c6551f86e14
Matthew Nelson <developer@matthewnelson.io>
2021-04-02 10:08:49 -0400

    create Contact presenter object

---
+++ sphinx/application/common/wrappers/wrapper-contact/src/main/java/chat/sphinx/wrapper_contact/Contact.kt
@@ -0,0 +1,31 @@
+package chat.sphinx.wrapper_contact
+
+import chat.sphinx.wrapper_common.DateTime
+import chat.sphinx.wrapper_common.Deleted
+import chat.sphinx.wrapper_common.PhotoUrl
+import chat.sphinx.wrapper_common.contact.ContactId
+import chat.sphinx.wrapper_common.invite.InviteId
+import chat.sphinx.wrapper_common.lightning.LightningNodeAlias
+import chat.sphinx.wrapper_common.lightning.LightningNodePubKey
+import chat.sphinx.wrapper_common.lightning.Sat
+import chat.sphinx.wrapper_relay.AuthorizationToken
+
+data class Contact(
+    val id: ContactId,
+    val pubKey: LightningNodePubKey,
+    val nodeAlias: LightningNodeAlias?,
+    val alias: ContactAlias,
+    val photoUrl: PhotoUrl?,
+    val privatePhoto: PrivatePhoto,
+    val isOwner: Owner,
+    val deleted: Deleted,
+    val authToken: AuthorizationToken?,
+    val status: ContactStatus,
+    val contactKey: ContactKey,
+    val deviceId: DeviceId,
+    val createdAt: DateTime,
+    val updatedAt: DateTime,
+    val notificationSound: NotificationSound?,
+    val tipAmount: Sat?,
+    val inviteId: InviteId?,
+)
