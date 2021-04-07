66d0f185e70723c959483b9e73cf0b4405a0ce75
Matthew Nelson <developer@matthewnelson.io>
2021-04-07 08:42:44 -0400

    reconfigure adapter to utilize the list of DashboardChats

--- sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/adapter/ChatListAdapter.kt
+++ sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/adapter/ChatListAdapter.kt
@@ -6,14 +6,13 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.*
-import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import app.cash.exhaustive.Exhaustive
 import chat.sphinx.dashboard.R
 import chat.sphinx.dashboard.ui.DashboardViewModel
-import chat.sphinx.wrapper_chat.Chat
 import chat.sphinx.wrapper_chat.ChatType
 import chat.sphinx.wrapper_chat.isMuted
 import io.matthewnelson.android_feature_screens.util.invisibleIfFalse
@@ -29,8 +28,8 @@ internal class ChatListAdapter(
     inner class ChatViewHolder(view: View): RecyclerView.ViewHolder(view)
 
     private inner class Diff(
-        private val oldList: List<Chat>,
-        private val newList: List<Chat>,
+        private val oldList: List<DashboardChat>,
+        private val newList: List<DashboardChat>,
     ): DiffUtil.Callback() {
         override fun getOldListSize(): Int {
             return oldList.size
@@ -42,7 +41,12 @@ internal class ChatListAdapter(
 
         override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
             return try {
-                oldList[oldItemPosition].id == newList[newItemPosition].id
+                oldList[oldItemPosition].let { old ->
+                    newList[newItemPosition].let { new ->
+                        old.chat.id == new.chat.id                              &&
+                        old.chat.latestMessageId == new.chat.latestMessageId
+                    }
+                }
             } catch (e: IndexOutOfBoundsException) {
                 false
             }
@@ -52,10 +56,10 @@ internal class ChatListAdapter(
             return try {
                 oldList[oldItemPosition].let { old ->
                     newList[newItemPosition].let { new ->
-                        old.isMuted == new.isMuted          &&
-                        old.seen == new.seen                &&
-                        old.name == new.name                &&
-                        old.photoUrl == new.photoUrl
+                        old.chat.isMuted == new.chat.isMuted                    &&
+                        old.chat.seen == new.chat.seen                          &&
+                        old.chat.name == new.chat.name                          &&
+                        old.chat.photoUrl == new.chat.photoUrl
                     }
                 }
             } catch (e: IndexOutOfBoundsException) {
@@ -65,26 +69,23 @@ internal class ChatListAdapter(
 
     }
 
-    private val chatsList = ArrayList<Chat>(viewModel.chatsStateFlow.value)
+    private val dashboardChats = ArrayList<DashboardChat>(viewModel.chatsStateFlow.value)
     private val supervisor = OnStartStopSupervisor(lifecycleOwner)
 
     override fun onStart(owner: LifecycleOwner) {
         super.onStart(owner)
         supervisor.scope().launch(viewModel.dispatchers.mainImmediate) {
-            viewModel.chatsStateFlow.collect { newChats ->
-
-                if (chatsList.isEmpty() && newChats.isNotEmpty()) {
-                    chatsList.addAll(newChats)
+            viewModel.chatsStateFlow.collect { newDashboardChats ->
+                if (dashboardChats.isEmpty() && newDashboardChats.isNotEmpty()) {
+                    dashboardChats.addAll(newDashboardChats)
                     this@ChatListAdapter.notifyDataSetChanged()
                 } else {
-                    withContext(viewModel.dispatchers.default) {
-                        DiffUtil.calculateDiff(
-                            Diff(chatsList, newChats)
-                        ).let { result ->
-                            chatsList.clear()
-                            chatsList.addAll(newChats)
-                            result.dispatchUpdatesTo(this@ChatListAdapter)
-                        }
+                    DiffUtil.calculateDiff(
+                        Diff(dashboardChats, newDashboardChats)
+                    ).let { result ->
+                        dashboardChats.clear()
+                        dashboardChats.addAll(newDashboardChats)
+                        result.dispatchUpdatesTo(this@ChatListAdapter)
                     }
                 }
             }
@@ -92,7 +93,7 @@ internal class ChatListAdapter(
     }
 
     override fun getItemCount(): Int {
-        return chatsList.size
+        return dashboardChats.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatListAdapter.ChatViewHolder {
@@ -104,7 +105,7 @@ internal class ChatListAdapter(
     }
 
     override fun onBindViewHolder(holder: ChatListAdapter.ChatViewHolder, position: Int) {
-        chatsList.getOrNull(position)?.let { chat ->
+        dashboardChats.getOrNull(position)?.let { dashboardChat ->
             val layoutChatHolder: ConstraintLayout =
                 holder.itemView.findViewById(R.id.layout_constraint_chat_holder)
             val imageViewChatHolder: ImageView =
@@ -120,28 +121,59 @@ internal class ChatListAdapter(
             val textViewTime: TextView =
                 holder.itemView.findViewById(R.id.text_view_chat_holder_time)
 
-            chat.name?.let {
-                textViewName.text = it.value
-            } // else contact name
+            @Exhaustive
+            when (dashboardChat.chat.type) {
+                is ChatType.Conversation -> {
+                    // use contact's name
+                    textViewName.text = "Contact"
+                    textViewName.setTextColor(
+                        ContextCompat.getColor(textViewName.context, android.R.color.white)
+                    )
+                }
+                is ChatType.Group,
+                is ChatType.Tribe -> {
+                    val name = dashboardChat.chat.name
 
-            imageViewNotification.invisibleIfFalse(chat.isMuted())
+                    textViewName.text = if (name != null) {
+                        textViewName.setTextColor(
+                            ContextCompat.getColor(textViewName.context, android.R.color.white)
+                        )
+                        name.value
+                    } else {
+                        textViewName.setTextColor(
+                            ContextCompat.getColor(textViewName.context, R.color.primaryRed)
+                        )
+                        "ERROR: NULL NAME"
+                    }
+                }
+                is ChatType.Unknown -> {
+                    textViewName.text = "ERROR: UNKNOWN CHAT TYPE"
+                    textViewName.setTextColor(
+                        ContextCompat.getColor(textViewName.context, R.color.primaryRed)
+                    )
+                }
+            }
+
+            textViewMessage.text = dashboardChat.chat.latestMessageId.toString()
+
+            imageViewNotification.invisibleIfFalse(dashboardChat.chat.isMuted())
 
             layoutChatHolder.setOnClickListener {
                 @Exhaustive
-                when (chat.type) {
+                when (dashboardChat.chat.type) {
                     is ChatType.Conversation -> {
                         supervisor.scope().launch {
-                            viewModel.dashboardNavigator.toChatContact(chat.id)
+                            viewModel.dashboardNavigator.toChatContact(dashboardChat.chat.id)
                         }
                     }
                     is ChatType.Group -> {
                         supervisor.scope().launch {
-                            viewModel.dashboardNavigator.toChatGroup(chat.id)
+                            viewModel.dashboardNavigator.toChatGroup(dashboardChat.chat.id)
                         }
                     }
                     is ChatType.Tribe -> {
                         supervisor.scope().launch {
-                            viewModel.dashboardNavigator.toChatTribe(chat.id)
+                            viewModel.dashboardNavigator.toChatTribe(dashboardChat.chat.id)
                         }
                     }
                     is ChatType.Unknown -> {
