5688645450da6a6fc832641e411f5be3e95cb8b0
Matthew Nelson <developer@matthewnelson.io>
2021-04-12 10:09:18 -0400

    add a Kotlin wrapper class for Coil's Disposable interface

--- sphinx/application/data/concepts/concept-image-loader/src/main/java/chat/sphinx/concept_image_loader/ImageLoader.kt
+++ sphinx/application/data/concepts/concept-image-loader/src/main/java/chat/sphinx/concept_image_loader/ImageLoader.kt
@@ -1,5 +1,26 @@
 package chat.sphinx.concept_image_loader
 
 abstract class ImageLoader<ImageView> {
-    abstract suspend fun load(imageView: ImageView, url: String)
+    abstract suspend fun load(imageView: ImageView, url: String): Disposable
+}
+
+/**
+ * Kotlin wrapper for Coil Android Library.
+ * */
+abstract class Disposable {
+
+    /**
+     * Returns true if the request is complete or cancelling.
+     */
+    abstract val isDisposed: Boolean
+
+    /**
+     * Cancels any in progress work and frees any resources associated with this request. This method is idempotent.
+     */
+    abstract fun dispose()
+
+    /**
+     * Suspends until any in progress work completes.
+     */
+    abstract suspend fun await()
 }
