09419f38533eced435053bf5fde83b9a8881c8ad
Matthew Nelson <developer@matthewnelson.io>
2021-04-14 18:34:14 -0400

    update DashboardNavigator to accept for navigating to a contact chat the nullable chat id, and non-null contact id

--- sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/navigation/DashboardNavigator.kt
+++ sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/navigation/DashboardNavigator.kt
@@ -2,13 +2,14 @@ package chat.sphinx.dashboard.navigation
 
 import androidx.navigation.NavController
 import chat.sphinx.wrapper_common.chat.ChatId
+import chat.sphinx.wrapper_common.contact.ContactId
 import io.matthewnelson.concept_navigation.BaseNavigationDriver
 import io.matthewnelson.concept_navigation.Navigator
 
 abstract class DashboardNavigator(
     navigationDriver: BaseNavigationDriver<NavController>
 ): Navigator<NavController>(navigationDriver) {
-    abstract suspend fun toChatContact(chatId: ChatId)
+    abstract suspend fun toChatContact(chatId: ChatId?, contactId: ContactId)
     abstract suspend fun toChatGroup(chatId: ChatId)
     abstract suspend fun toChatTribe(chatId: ChatId)
 }
