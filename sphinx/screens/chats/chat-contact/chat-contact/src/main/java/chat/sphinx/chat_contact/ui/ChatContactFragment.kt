c45808d21e7132f39c3a741cee01e6deacf64922
Matthew Nelson <developer@matthewnelson.io>
2021-04-16 14:56:00 -0400

    implement base chat components into contact chat

--- sphinx/screens/chats/chat-contact/chat-contact/src/main/java/chat/sphinx/chat_contact/ui/ChatContactFragment.kt
+++ sphinx/screens/chats/chat-contact/chat-contact/src/main/java/chat/sphinx/chat_contact/ui/ChatContactFragment.kt
@@ -1,23 +1,26 @@
 package chat.sphinx.chat_contact.ui
 
-import androidx.fragment.app.viewModels
+import androidx.constraintlayout.widget.ConstraintLayout
 import by.kirich1409.viewbindingdelegate.viewBinding
+import chat.sphinx.chat_common.BaseChatFragment
+import chat.sphinx.chat_common.ChatViewState
 import chat.sphinx.chat_contact.R
 import chat.sphinx.chat_contact.databinding.FragmentChatContactBinding
 import dagger.hilt.android.AndroidEntryPoint
-import io.matthewnelson.android_feature_screens.ui.base.BaseFragment
 
 @AndroidEntryPoint
-internal class ChatContactFragment: BaseFragment<
-        ChatContactViewState,
-        ChatContactViewModel,
+internal class ChatContactFragment: BaseChatFragment<
         FragmentChatContactBinding
         >(R.layout.fragment_chat_contact)
 {
-    override val viewModel: ChatContactViewModel by viewModels()
     override val binding: FragmentChatContactBinding by viewBinding(FragmentChatContactBinding::bind)
 
-    override suspend fun onViewStateFlowCollect(viewState: ChatContactViewState) {
+    override val footer: ConstraintLayout
+        get() = binding.layoutChatFooter.layoutConstraintChatFooter
+    override val header: ConstraintLayout
+        get() = binding.layoutChatHeader.layoutConstraintChatHeader
+
+    override suspend fun onViewStateFlowCollect(viewState: ChatViewState) {
 //        TODO("Not yet implemented")
     }
 }
