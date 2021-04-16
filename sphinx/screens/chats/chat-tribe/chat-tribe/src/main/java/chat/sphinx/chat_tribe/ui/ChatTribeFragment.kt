84192860c8ca87b7ca5c50d73f4f3ecd069cacca
Matthew Nelson <developer@matthewnelson.io>
2021-04-16 14:56:40 -0400

    implement base chat components into tribe chat

--- sphinx/screens/chats/chat-tribe/chat-tribe/src/main/java/chat/sphinx/chat_tribe/ui/ChatTribeFragment.kt
+++ sphinx/screens/chats/chat-tribe/chat-tribe/src/main/java/chat/sphinx/chat_tribe/ui/ChatTribeFragment.kt
@@ -1,23 +1,25 @@
 package chat.sphinx.chat_tribe.ui
 
-import androidx.fragment.app.viewModels
+import androidx.constraintlayout.widget.ConstraintLayout
 import by.kirich1409.viewbindingdelegate.viewBinding
+import chat.sphinx.chat_common.BaseChatFragment
+import chat.sphinx.chat_common.ChatViewState
 import chat.sphinx.chat_tribe.R
 import chat.sphinx.chat_tribe.databinding.FragmentChatTribeBinding
 import dagger.hilt.android.AndroidEntryPoint
-import io.matthewnelson.android_feature_screens.ui.base.BaseFragment
 
 @AndroidEntryPoint
-internal class ChatTribeFragment: BaseFragment<
-        ChatTribeViewState,
-        ChatTribeViewModel,
+internal class ChatTribeFragment: BaseChatFragment<
         FragmentChatTribeBinding
         >(R.layout.fragment_chat_tribe)
 {
-    override val viewModel: ChatTribeViewModel by viewModels()
     override val binding: FragmentChatTribeBinding by viewBinding(FragmentChatTribeBinding::bind)
+    override val header: ConstraintLayout
+        get() = binding.layoutChatHeader.layoutConstraintChatHeader
+    override val footer: ConstraintLayout
+        get() = binding.layoutChatFooter.layoutConstraintChatFooter
 
-    override suspend fun onViewStateFlowCollect(viewState: ChatTribeViewState) {
+    override suspend fun onViewStateFlowCollect(viewState: ChatViewState) {
 //        TODO("Not yet implemented")
     }
 }
--- sphinx/screens/chats/chat-tribe/chat-tribe/src/main/java/chat/sphinx/chat_tribe/ui/ChatTribeViewState.kt
+++
@@ -1,7 +0,0 @@
-package chat.sphinx.chat_tribe.ui
-
-import io.matthewnelson.concept_views.viewstate.ViewState
-
-internal sealed class ChatTribeViewState: ViewState<ChatTribeViewState>() {
-    object Idle: ChatTribeViewState()
-}
