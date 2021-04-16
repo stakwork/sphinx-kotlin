477843ed9a601d53b06d4999aaddffe6ddf1f26b
Matthew Nelson <developer@matthewnelson.io>
2021-04-16 14:56:22 -0400

    implement base chat components into group chat

--- sphinx/screens/chats/chat-group/chat-group/src/main/java/chat/sphinx/chat_group/ui/ChatGroupFragment.kt
+++ sphinx/screens/chats/chat-group/chat-group/src/main/java/chat/sphinx/chat_group/ui/ChatGroupFragment.kt
@@ -1,23 +1,27 @@
 package chat.sphinx.chat_group.ui
 
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
+import chat.sphinx.chat_common.BaseChatFragment
+import chat.sphinx.chat_common.ChatViewState
 import chat.sphinx.chat_group.R
 import chat.sphinx.chat_group.databinding.FragmentChatGroupBinding
 import dagger.hilt.android.AndroidEntryPoint
 import io.matthewnelson.android_feature_screens.ui.base.BaseFragment
 
 @AndroidEntryPoint
-internal class ChatGroupFragment: BaseFragment<
-        ChatGroupViewState,
-        ChatGroupViewModel,
+internal class ChatGroupFragment: BaseChatFragment<
         FragmentChatGroupBinding
         >(R.layout.fragment_chat_group)
 {
-    override val viewModel: ChatGroupViewModel by viewModels()
     override val binding: FragmentChatGroupBinding by viewBinding(FragmentChatGroupBinding::bind)
+    override val footer: ConstraintLayout
+        get() = binding.layoutChatFooter.layoutConstraintChatFooter
+    override val header: ConstraintLayout
+        get() = binding.layoutChatHeader.layoutConstraintChatHeader
 
-    override suspend fun onViewStateFlowCollect(viewState: ChatGroupViewState) {
+    override suspend fun onViewStateFlowCollect(viewState: ChatViewState) {
 //        TODO("Not yet implemented")
     }
 }
