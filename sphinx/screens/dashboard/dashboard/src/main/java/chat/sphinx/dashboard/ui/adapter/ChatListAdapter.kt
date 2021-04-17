c5957656efc42fcfd85530fc6fc40cef8ee06b66
Matthew Nelson <developer@matthewnelson.io>
2021-04-17 07:37:35 -0400

    reduce dashboard recycler view jitter by setting onclick listener only once

--- sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/adapter/ChatListAdapter.kt
+++ sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/adapter/ChatListAdapter.kt
@@ -181,10 +181,52 @@ internal class ChatListAdapter(
     ): RecyclerView.ViewHolder(binding.root) {
 
         private var disposable: Disposable? = null
+        private var dChat: DashboardChat? = null
+
+        init {
+            binding.layoutConstraintChatHolder.setOnClickListener {
+                dChat?.let { dashboardChat ->
+                    @Exhaustive
+                    when (dashboardChat) {
+                        is DashboardChat.Active.Conversation -> {
+                            supervisor.scope().launch(viewModel.dispatchers.mainImmediate) {
+                                viewModel.dashboardNavigator.toChatContact(
+                                    dashboardChat.chat,
+                                    dashboardChat.contact
+                                )
+                            }
+                        }
+                        is DashboardChat.Active.GroupOrTribe -> {
+                            supervisor.scope().launch(viewModel.dispatchers.mainImmediate) {
+
+                                if (dashboardChat.chat.type.isGroup()) {
+                                    viewModel.dashboardNavigator.toChatGroup(dashboardChat.chat)
+                                } else if (dashboardChat.chat.type.isTribe()) {
+                                    viewModel.dashboardNavigator.toChatTribe(dashboardChat.chat)
+                                }
+
+                            }
+                        }
+                        is DashboardChat.Inactive.Conversation -> {
+                            supervisor.scope().launch(viewModel.dispatchers.mainImmediate) {
+                                viewModel.dashboardNavigator.toChatContact(
+                                    null,
+                                    dashboardChat.contact
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         fun bind(position: Int) {
             binding.apply {
-                val dashboardChat = dashboardChats.getOrNull(position) ?: return
+                val dashboardChat: DashboardChat = dashboardChats.getOrNull(position) ?: let {
+                    dChat = null
+                    return
+                }
+                dChat = dashboardChat
                 disposable?.dispose()
 
                 // Set Defaults
@@ -244,40 +286,6 @@ internal class ChatListAdapter(
                     imageViewChatHolderNotification.invisibleIfFalse(dashboardChat.chat.isMuted())
                 } else {
                     imageViewChatHolderNotification.invisibleIfFalse(false)
-                }
-
-                // ClickListener
-                layoutConstraintChatHolder.setOnClickListener {
-                    @Exhaustive
-                    when (dashboardChat) {
-                        is DashboardChat.Active.Conversation -> {
-                            supervisor.scope().launch(viewModel.dispatchers.mainImmediate) {
-                                viewModel.dashboardNavigator.toChatContact(
-                                    dashboardChat.chat,
-                                    dashboardChat.contact
-                                )
-                            }
-                        }
-                        is DashboardChat.Active.GroupOrTribe -> {
-                            supervisor.scope().launch(viewModel.dispatchers.mainImmediate) {
-
-                                if (dashboardChat.chat.type.isGroup()) {
-                                    viewModel.dashboardNavigator.toChatGroup(dashboardChat.chat)
-                                } else if (dashboardChat.chat.type.isTribe()) {
-                                    viewModel.dashboardNavigator.toChatTribe(dashboardChat.chat)
-                                }
-
-                            }
-                        }
-                        is DashboardChat.Inactive.Conversation -> {
-                            supervisor.scope().launch(viewModel.dispatchers.mainImmediate) {
-                                viewModel.dashboardNavigator.toChatContact(
-                                    null,
-                                    dashboardChat.contact
-                                )
-                            }
-                        }
-                    }
                 }
             }
         }
