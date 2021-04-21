8e0808c7f6475ca657cf24aa2949659494ea06ea
Matthew Nelson <developer@matthewnelson.io>
2021-04-21 10:38:14 -0400

    move extension function for retrieving random color value out on it's own

--- sphinx/application/common/resources/src/main/java/chat/sphinx/resources/ContextExtensions.kt
+++ sphinx/application/common/resources/src/main/java/chat/sphinx/resources/ContextExtensions.kt
@@ -2,11 +2,43 @@ package chat.sphinx.resources
 
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 import chat.sphinx.wrapper_view.Dp
 import chat.sphinx.wrapper_view.Px
 
 inline val Context.inputMethodManager: InputMethodManager?
     get() = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+
+@ColorInt
+@Suppress("NOTHING_TO_INLINE")
+inline fun Context.getRandomColor(): Int {
+    val randomColorResource = listOf<@ColorRes Int>(
+        R.color.randomColor1,
+        R.color.randomColor2,
+        R.color.randomColor3,
+        R.color.randomColor4,
+        R.color.randomColor5,
+        R.color.randomColor6,
+        R.color.randomColor7,
+        R.color.randomColor8,
+        R.color.randomColor9,
+        R.color.randomColor10,
+        R.color.randomColor11,
+        R.color.randomColor12,
+        R.color.randomColor13,
+        R.color.randomColor14,
+        R.color.randomColor15,
+        R.color.randomColor16,
+        R.color.randomColor17,
+        R.color.randomColor18,
+        R.color.randomColor19,
+        R.color.randomColor20,
+    ).shuffled()[0]
+
+    return ContextCompat.getColor(this, randomColorResource)
+}
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun Dp.toPx(context: Context): Px =
--- sphinx/application/common/resources/src/main/java/chat/sphinx/resources/ViewExtensions.kt
+++ sphinx/application/common/resources/src/main/java/chat/sphinx/resources/ViewExtensions.kt
@@ -1,7 +1,10 @@
 package chat.sphinx.resources
 
+import android.content.Context
+import android.os.Build
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
@@ -16,44 +19,25 @@ inline fun TextView.setTextColorExt(@ColorRes resId: Int): TextView {
 
 /**
  * Providing a Drawable [resId] will retrieve the drawable and
- * set it's tint to the random, then set the view's background
+ * set it's tint to the random color, then set the view's background
  * to that drawable.
  *
  * Other wise it sets the view's background to the random color.
+ *
+ * Alternatively, you can pass a value for [color] which provides
+ * flexibility if you want to use a specific random color throughout
+ * a given screen.
  * */
 @Suppress("NOTHING_TO_INLINE")
-inline fun View.setBackgroundRandomColor(@DrawableRes resId: Int?) {
-    val randomColorResource = listOf<@ColorRes Int>(
-        R.color.randomColor1,
-        R.color.randomColor2,
-        R.color.randomColor3,
-        R.color.randomColor4,
-        R.color.randomColor5,
-        R.color.randomColor6,
-        R.color.randomColor7,
-        R.color.randomColor8,
-        R.color.randomColor9,
-        R.color.randomColor10,
-        R.color.randomColor11,
-        R.color.randomColor12,
-        R.color.randomColor13,
-        R.color.randomColor14,
-        R.color.randomColor15,
-        R.color.randomColor16,
-        R.color.randomColor17,
-        R.color.randomColor18,
-        R.color.randomColor19,
-        R.color.randomColor20,
-    ).shuffled()[0]
-
-    val color = ContextCompat.getColor(this.context, randomColorResource)
-
+inline fun View.setBackgroundRandomColor(
+    @DrawableRes resId: Int?,
+    @ColorInt color: Int? = null
+) {
     if (resId != null) {
         val drawable = ContextCompat.getDrawable(this.context, resId)
-        drawable?.setTint(color)
+        drawable?.setTint(color ?: this.context.getRandomColor())
         this.background = drawable
     } else {
-        this.setBackgroundColor(color)
+        this.setBackgroundColor(color ?: this.context.getRandomColor())
     }
-
 }
