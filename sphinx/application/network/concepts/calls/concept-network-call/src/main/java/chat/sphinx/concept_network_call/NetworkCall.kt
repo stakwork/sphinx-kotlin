63b6950e7241dbbb5774ae29efbf4e9cf03f3dec
Matthew Nelson <developer@matthewnelson.io>
2021-05-04 13:47:07 -0400

    initialize network-call module for handling non-Relay GET/POST/PUT/DELETE network queries

---
+++ sphinx/application/network/concepts/calls/concept-network-call/src/main/java/chat/sphinx/concept_network_call/NetworkCall.kt
@@ -0,0 +1,6 @@
+package chat.sphinx.concept_network_call
+
+/**
+ * Methods for GET/PUT/POST/DELETE for general, non-Relay specific network queries.
+ * */
+abstract class NetworkCall {}
--- sphinx/application/network/concepts/calls/concept-network-relay-call/src/main/java/chat/sphinx/concept_network_relay_call/NetworkRelayCall.kt
+++ sphinx/application/network/concepts/calls/concept-network-relay-call/src/main/java/chat/sphinx/concept_network_relay_call/NetworkRelayCall.kt
@@ -1,12 +1,19 @@
 package chat.sphinx.concept_network_relay_call
 
+import chat.sphinx.concept_network_call.NetworkCall
 import chat.sphinx.kotlin_response.LoadResponse
 import chat.sphinx.kotlin_response.ResponseError
 import chat.sphinx.wrapper_relay.AuthorizationToken
 import chat.sphinx.wrapper_relay.RelayUrl
 import kotlinx.coroutines.flow.Flow
 
-abstract class NetworkRelayCall {
+/**
+ * Methods for GET/PUT/POST/DELETE that are specific to interacting with Relay.
+ *
+ * It automatically adds Headers for the Account Owner's [AuthorizationToken],
+ * and handles success/errors depending on [RelayResponse.success].
+ * */
+abstract class NetworkRelayCall: NetworkCall() {
 
     /**
      * GET
