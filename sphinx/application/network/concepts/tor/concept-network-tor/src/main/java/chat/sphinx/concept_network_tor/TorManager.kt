227da1fc95e0bd1c6769e788fc64535d32502615
Matthew Nelson <developer@matthewnelson.io>
2021-05-06 11:08:00 -0400

    initialize Tor concept/feature modules

---
+++ sphinx/application/network/concepts/tor/concept-network-tor/src/main/java/chat/sphinx/concept_network_tor/TorManager.kt
@@ -0,0 +1,4 @@
+package chat.sphinx.concept_network_tor
+
+abstract class TorManager {
+}
---
+++ sphinx/application/network/features/tor/feature-network-tor/src/main/java/chat/sphinx/feature_network_tor/TorManagerAndroid.kt
@@ -0,0 +1,6 @@
+package chat.sphinx.feature_network_tor
+
+import chat.sphinx.concept_network_tor.TorManager
+
+class TorManagerAndroid: TorManager() {
+}
