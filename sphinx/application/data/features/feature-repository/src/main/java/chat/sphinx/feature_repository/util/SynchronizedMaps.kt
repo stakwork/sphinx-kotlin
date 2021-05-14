9e5ccf63f5ba96a655ca384ee674736cbc220f41
Matthew Nelson <developer@matthewnelson.io>
2021-05-14 14:04:40 -0400

    add synchronized map wrapper classes

---
+++ sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/util/SynchronizedMaps.kt
@@ -0,0 +1,28 @@
+package chat.sphinx.feature_repository.util
+
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+/**
+ * Wraps a mutable map in a suspend function such that anything
+ * executed within the lambda holds a lock.
+ * */
+class SynchronizedSuspendMap<K, V>(initialCapacity: Int = 0) {
+    private val map: MutableMap<K, V> = LinkedHashMap(initialCapacity)
+    private val lock = Mutex()
+
+    suspend fun<T> withLock(action: (MutableMap<K, V>) -> T): T =
+        lock.withLock { action(map) }
+}
+
+/**
+ * Wraps a mutable map in a suspend function such that anything
+ * executed within the lambda holds a lock.
+ * */
+class SynchronizedMap<K, V>(initialCapacity: Int = 0) {
+    private val map: MutableMap<K, V> = LinkedHashMap(initialCapacity)
+    private val lock = Object()
+
+    fun<T> withLock(action: (MutableMap<K, V>) -> T): T =
+        synchronized(lock) { action(map) }
+}
