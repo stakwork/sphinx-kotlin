a7682d594df0992a55352cc7dda7666d1e7defba
Matthew Nelson <developer@matthewnelson.io>
2021-05-16 18:27:48 -0400

    Implement scanner view model coordinator into new contact screen

--- sphinx/screens-detail/new-contact/new-contact/src/main/java/chat/sphinx/new_contact/ui/NewContactViewModel.kt
+++ sphinx/screens-detail/new-contact/new-contact/src/main/java/chat/sphinx/new_contact/ui/NewContactViewModel.kt
@@ -3,9 +3,13 @@ package chat.sphinx.new_contact.ui
 import android.content.Context
 import androidx.lifecycle.viewModelScope
 import chat.sphinx.concept_repository_contact.ContactRepository
+import chat.sphinx.concept_view_model_coordinator.ViewModelCoordinator
 import chat.sphinx.kotlin_response.LoadResponse
 import chat.sphinx.kotlin_response.Response
 import chat.sphinx.new_contact.navigation.NewContactNavigator
+import chat.sphinx.scanner_view_model_coordinator.request.ScannerFilter
+import chat.sphinx.scanner_view_model_coordinator.request.ScannerRequest
+import chat.sphinx.scanner_view_model_coordinator.response.ScannerResponse
 import chat.sphinx.wrapper_common.lightning.LightningNodePubKey
 import chat.sphinx.wrapper_common.lightning.LightningRouteHint
 import chat.sphinx.wrapper_common.lightning.toLightningNodePubKey
@@ -26,12 +30,37 @@ internal class NewContactViewModel @Inject constructor(
     val navigator: NewContactNavigator,
     dispatchers: CoroutineDispatchers,
     private val contactRepository: ContactRepository,
+    private val scannerCoordinator: ViewModelCoordinator<ScannerRequest, ScannerResponse>
 ): SideEffectViewModel<
         Context,
         NewContactSideEffect,
         NewContactViewState
         >(dispatchers, NewContactViewState.Idle)
 {
+    fun requestScanner() {
+        viewModelScope.launch(mainImmediate) {
+            val response = scannerCoordinator.submitRequest(
+                ScannerRequest(
+                    filter = object : ScannerFilter() {
+                        override suspend fun checkData(data: String): Response<Any, String> {
+                            if (data.trim().matches(NewContactFragment.PASTE_REGEX.toRegex())) {
+                                return Response.Success(Any())
+                            }
+                            if (data.toLightningNodePubKey() != null) {
+                                return Response.Success(Any())
+                            }
+
+                            return Response.Error("QR code is not a Lightning Node Public Key")
+                        }
+                    }
+                )
+            )
+            if (response is Response.Success) {
+                submitSideEffect(NewContactSideEffect.FromScanner(response.value))
+            }
+        }
+    }
+
     fun addContact(
         contactAlias: String,
         lightningNodePubKey: String,
@@ -39,11 +68,11 @@ internal class NewContactViewModel @Inject constructor(
     ) {
         viewModelScope.launch(mainImmediate) {
             val alias: ContactAlias = contactAlias.trim().toContactAlias() ?: let {
-                submitSideEffect(NewContactSideEffect.NicknameAndAddressRequired)
+                submitSideEffect(NewContactSideEffect.Notify.NicknameAndAddressRequired)
                 return@launch
             }
             val pubKey: LightningNodePubKey = lightningNodePubKey.trim().toLightningNodePubKey() ?: let {
-                submitSideEffect(NewContactSideEffect.InvalidLightningNodePublicKey)
+                submitSideEffect(NewContactSideEffect.Notify.InvalidLightningNodePublicKey)
                 return@launch
             }
             val routeHint: LightningRouteHint? = lightningRouteHint?.trim()?.let {
@@ -54,7 +83,7 @@ internal class NewContactViewModel @Inject constructor(
                 } else {
                     val hint = it.toLightningRouteHint()
                     if (hint == null) {
-                        submitSideEffect(NewContactSideEffect.InvalidRouteHint)
+                        submitSideEffect(NewContactSideEffect.Notify.InvalidRouteHint)
                         return@launch
                     } else {
                         hint
