b3d5b86076f14aa5ea560c93690906f43049d868
Matthew Nelson <developer@matthewnelson.io>
2021-05-16 17:02:23 -0400

    implement ScannerViewModelCoordinator into scanner module's architecture components

--- sphinx/screens-detail/scanner/scanner/src/main/java/chat/sphinx/scanner/ui/ScannerFragment.kt
+++ sphinx/screens-detail/scanner/scanner/src/main/java/chat/sphinx/scanner/ui/ScannerFragment.kt
@@ -1,9 +1,12 @@
 package chat.sphinx.scanner.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import chat.sphinx.scanner.R
 import chat.sphinx.scanner.databinding.FragmentScannerBinding
+import chat.sphinx.scanner.navigation.BackType
 import dagger.hilt.android.AndroidEntryPoint
 import io.matthewnelson.android_feature_screens.ui.base.BaseFragment
 
@@ -16,6 +19,20 @@ internal class ScannerFragment: BaseFragment<
 {
     override val viewModel: ScannerViewModel by viewModels()
     override val binding: FragmentScannerBinding by viewBinding(FragmentScannerBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // have to call it here so it gets injected and can
+        // catch the request asap
+        viewModel
+
+        binding.textViewScannerClose.setOnClickListener {
+            viewModel.goBack(BackType.CloseDetailScreen)
+        }
+        binding.textViewScannerHeaderNavBack.setOnClickListener {
+            viewModel.goBack(BackType.PopBackStack)
+        }
+    }
 
     override suspend fun onViewStateFlowCollect(viewState: ScannerViewState) {
 //        TODO("Not yet implemented")
