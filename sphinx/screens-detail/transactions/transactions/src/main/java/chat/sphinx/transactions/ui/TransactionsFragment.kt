f239078b26d300e8c36ae74081ee9347da730891
Matthew Nelson <developer@matthewnelson.io>
2021-05-17 06:25:00 -0400

    update Transactions detail screen to use common header layout

--- sphinx/screens-detail/transactions/transactions/src/main/java/chat/sphinx/transactions/ui/TransactionsFragment.kt
+++ sphinx/screens-detail/transactions/transactions/src/main/java/chat/sphinx/transactions/ui/TransactionsFragment.kt
@@ -1,5 +1,7 @@
 package chat.sphinx.transactions.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import chat.sphinx.transactions.R
@@ -16,6 +18,16 @@ internal class TransactionsFragment: BaseFragment<
 {
     override val viewModel: TransactionsViewModel by viewModels()
     override val binding: FragmentTransactionsBinding by viewBinding(FragmentTransactionsBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.includeTransactionsHeader.apply {
+            textViewDetailScreenHeaderName.text = getString(R.string.transactions_header_name)
+//            textViewDetailScreenClose.setOnClickListener {
+            // TODO: Navigate
+//            }
+        }
+    }
 
     override suspend fun onViewStateFlowCollect(viewState: TransactionsViewState) {
 //        TODO("Not yet implemented")
