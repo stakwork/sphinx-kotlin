ea804c273e2432617b166257db3570aa57f82a04
Matthew Nelson <developer@matthewnelson.io>
2021-05-17 06:23:53 -0400

    update PaymentSend detail screen to use common header layout

--- sphinx/screens-detail/payments/payment-send/payment-send/src/main/java/chat/sphinx/payment_send/ui/PaymentSendFragment.kt
+++ sphinx/screens-detail/payments/payment-send/payment-send/src/main/java/chat/sphinx/payment_send/ui/PaymentSendFragment.kt
@@ -1,5 +1,7 @@
 package chat.sphinx.payment_send.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import chat.sphinx.payment_send.R
@@ -16,6 +18,16 @@ internal class PaymentSendFragment: BaseFragment<
 {
     override val viewModel: PaymentSendViewModel by viewModels()
     override val binding: FragmentPaymentSendBinding by viewBinding(FragmentPaymentSendBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.includePaymentSendHeader.apply {
+            textViewDetailScreenHeaderName.text = getString(R.string.payment_send_header_name)
+//            textViewDetailScreenClose.setOnClickListener {
+            // TODO: Navigate
+//            }
+        }
+    }
 
     override suspend fun onViewStateFlowCollect(viewState: PaymentSendViewState) {
 //        TODO("Not yet implemented")
