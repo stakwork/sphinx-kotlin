6dce411a81c2f2d72981237919ddd72504cc3ab9
Matthew Nelson <developer@matthewnelson.io>
2021-05-17 06:24:41 -0400

    update SupportTicket detail screen to use common header layout

--- sphinx/screens-detail/support-ticket/support-ticket/src/main/java/chat/sphinx/support_ticket/ui/SupportTicketFragment.kt
+++ sphinx/screens-detail/support-ticket/support-ticket/src/main/java/chat/sphinx/support_ticket/ui/SupportTicketFragment.kt
@@ -1,5 +1,7 @@
 package chat.sphinx.support_ticket.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import chat.sphinx.support_ticket.R
@@ -16,6 +18,16 @@ internal class SupportTicketFragment: BaseFragment<
 {
     override val viewModel: SupportTicketViewModel by viewModels()
     override val binding: FragmentSupportTicketBinding by viewBinding(FragmentSupportTicketBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.includeSupportTicketHeader.apply {
+            textViewDetailScreenHeaderName.text = getString(R.string.support_ticket_header_name)
+//            textViewDetailScreenClose.setOnClickListener {
+            // TODO: Navigate
+//            }
+        }
+    }
 
     override suspend fun onViewStateFlowCollect(viewState: SupportTicketViewState) {
 //        TODO("Not yet implemented")
