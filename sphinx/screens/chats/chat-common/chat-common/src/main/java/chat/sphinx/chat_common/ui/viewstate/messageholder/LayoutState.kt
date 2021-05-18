40f6b2bfc610d5bb3d57dbef560191b530399b6b
Matthew Nelson <developer@matthewnelson.io>
2021-05-18 09:24:10 -0400

    remove duplicate reference to message (already present in the MessageHolderViewState)

--- sphinx/screens/chats/chat-common/chat-common/src/main/java/chat/sphinx/chat_common/ui/viewstate/messageholder/LayoutState.kt
+++ sphinx/screens/chats/chat-common/chat-common/src/main/java/chat/sphinx/chat_common/ui/viewstate/messageholder/LayoutState.kt
@@ -3,14 +3,13 @@ package chat.sphinx.chat_common.ui.viewstate.messageholder
 import chat.sphinx.wrapper_common.lightning.Sat
 import chat.sphinx.wrapper_common.lightning.unit
 import chat.sphinx.wrapper_message.Message
+import chat.sphinx.wrapper_message.MessageContentDecrypted
 
 sealed class LayoutState {
 
     data class MessageTypeMessageContent(
-        val message: Message,
-    ): LayoutState() {
-        val messageText: String? = message.messageContentDecrypted?.value
-    }
+        val messageContent: MessageContentDecrypted
+    ): LayoutState()
 
     data class DirectPayment(
         val showSent: Boolean,
