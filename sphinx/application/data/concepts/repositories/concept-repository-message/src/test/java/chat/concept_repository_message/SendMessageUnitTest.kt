de9cc57d5760509835bed4b86333ac102bbca8f6
Matthew Nelson <developer@matthewnelson.io>
2021-05-21 09:10:04 -0400

    create a send message + builder class

---
+++ sphinx/application/data/concepts/repositories/concept-repository-message/src/test/java/chat/concept_repository_message/SendMessageUnitTest.kt
@@ -0,0 +1,70 @@
+package chat.concept_repository_message
+
+import chat.sphinx.concept_repository_message.SendMessage
+import chat.sphinx.wrapper_common.chat.ChatId
+import chat.sphinx.wrapper_common.contact.ContactId
+import org.junit.After
+import org.junit.Assert
+import org.junit.Test
+import java.io.File
+
+class SendMessageUnitTest {
+
+    private val builder = SendMessage.Builder()
+    private val assertFalse
+        get() = Assert.assertFalse(builder.isValid)
+
+    private val assertTrue
+        get() = Assert.assertTrue(builder.isValid)
+
+    @After
+    fun tearDown() {
+        builder.clear()
+    }
+
+    @Test
+    fun `no contact or chat id fails`() {
+        builder.setText("some text")
+        assertFalse
+
+        builder.setFile(File(System.getProperty("user.dir")))
+        assertFalse
+    }
+
+    @Test
+    fun `contact and or chat id succeeds`() {
+        builder.setText("some text")
+
+        // only contact succeeds
+        builder.setContactId(ContactId(1))
+        assertTrue
+
+        // both contact & chat succeeds
+        builder.setChatId(ChatId(1))
+        assertTrue
+
+        // only chat succeeds
+        builder.setContactId(null)
+        assertTrue
+
+        builder.setChatId(null)
+        assertFalse
+    }
+
+    @Test
+    fun `file DNE fails`() {
+        val path = System.getProperty("user.dir")
+        // invalid file path
+        builder.setFile(File(path.dropLast(3)))
+        builder.setContactId(ContactId(1))
+        assertFalse
+
+        // fails even with text b/c file is invalid
+        builder.setText("some text")
+        assertFalse
+
+        // file exists
+        builder.setFile(File(path))
+        assertTrue
+    }
+}
