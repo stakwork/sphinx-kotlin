237b0b902446e23d5e52ffb8ad6db41cb184d504
Matthew Nelson <developer@matthewnelson.io>
2021-05-27 13:55:28 -0400

    consolodate all current dashboard repository calls in RepositoryDashboard

---
+++ sphinx/application/data/concepts/repositories/concept-repository-dashboard/src/main/java/chat/sphinx/concept_repository_dashboard/RepositoryDashboard.kt
@@ -0,0 +1,30 @@
+package chat.sphinx.concept_repository_dashboard
+
+import chat.sphinx.kotlin_response.LoadResponse
+import chat.sphinx.kotlin_response.ResponseError
+import chat.sphinx.wrapper_chat.Chat
+import chat.sphinx.wrapper_common.dashboard.ContactId
+import chat.sphinx.wrapper_common.message.MessageId
+import chat.sphinx.wrapper_contact.Contact
+import chat.sphinx.wrapper_lightning.NodeBalance
+import chat.sphinx.wrapper_message.Message
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+interface RepositoryDashboard {
+    suspend fun getAccountBalance(): StateFlow<NodeBalance?>
+
+    val getAllChats: Flow<List<Chat>>
+    fun getUnseenMessagesByChatId(chat: Chat): Flow<Long?>
+
+    val accountOwner: StateFlow<Contact?>
+    val getAllContacts: Flow<List<Contact>>
+    fun getContactById(contactId: ContactId): Flow<Contact?>
+    var updatedContactIds: MutableList<ContactId>
+
+    fun getMessageById(messageId: MessageId): Flow<Message?>
+
+    val networkRefreshBalance: Flow<LoadResponse<Boolean, ResponseError>>
+    val networkRefreshContacts: Flow<LoadResponse<Boolean, ResponseError>>
+    val networkRefreshMessages: Flow<LoadResponse<Boolean, ResponseError>>
+}
