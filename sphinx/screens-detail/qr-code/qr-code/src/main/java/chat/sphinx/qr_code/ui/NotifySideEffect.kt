737606379dcdcf9629a0a1718af5b895d8ae2098
Matthew Nelson <developer@matthewnelson.io>
2021-05-29 07:40:58 -0400

    add internal visibility modifier

--- sphinx/screens-detail/qr-code/qr-code/src/main/java/chat/sphinx/qr_code/ui/NotifySideEffect.kt
+++ sphinx/screens-detail/qr-code/qr-code/src/main/java/chat/sphinx/qr_code/ui/NotifySideEffect.kt
@@ -5,7 +5,7 @@ import chat.sphinx.resources.SphinxToastUtils
 import io.matthewnelson.android_feature_toast_utils.show
 import io.matthewnelson.concept_views.sideeffect.SideEffect
 
-class NotifySideEffect(val value: String): SideEffect<Context>() {
+internal class NotifySideEffect(val value: String): SideEffect<Context>() {
     override suspend fun execute(value: Context) {
         SphinxToastUtils().show(value, this.value)
     }
