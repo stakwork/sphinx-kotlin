20c061642c5d162f04f0a2a99dd214f699efb715
CypherPoet <CypherPoet@gmail.com>
2021-06-02 09:34:47 -0500

    tweak navigator naming and remove unecessary comments

--- sphinx/screens-detail/chat-tribe/podcast-player/src/main/java/chat/sphinx/chat_tribe/podcast_player/ui/TribeChatPodcastPlayerFragment.kt
+++ sphinx/screens-detail/chat-tribe/podcast-player/src/main/java/chat/sphinx/chat_tribe/podcast_player/ui/TribeChatPodcastPlayerFragment.kt
@@ -8,15 +8,15 @@ import chat.sphinx.chat_tribe.podcast_player.R
 import chat.sphinx.chat_tribe.podcast_player.databinding.FragmentTribeChatPodcastPlayerBinding
 import dagger.hilt.android.AndroidEntryPoint
 import io.matthewnelson.android_feature_screens.ui.base.BaseFragment
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
-internal class TribeChatPodcastPlayerFragment: BaseFragment<
+internal class TribeChatPodcastPlayerFragment : BaseFragment<
         TribeChatPodcastPlayerViewState,
         TribeChatPodcastPlayerViewModel,
         FragmentTribeChatPodcastPlayerBinding
-        >(R.layout.fragment_tribe_chat_podcast_player)
-{
+        >(R.layout.fragment_tribe_chat_podcast_player) {
     override val viewModel: TribeChatPodcastPlayerViewModel by viewModels()
     override val binding: FragmentTribeChatPodcastPlayerBinding by viewBinding(FragmentTribeChatPodcastPlayerBinding::bind)
 
@@ -27,12 +27,6 @@ internal class TribeChatPodcastPlayerFragment: BaseFragment<
 //            textViewDetailScreenClose.setOnClickListener {
 //                lifecycleScope.launch { viewModel.navigator.closeDetailScreen() }
 //            }
-//        }
-//        binding.buttonNewToSphinx.setOnClickListener {
-//            lifecycleScope.launch { viewModel.navigator.toCreateInvitationDetail() }
-//        }
-//        binding.buttonAlreadyOnSphinx.setOnClickListener {
-//            lifecycleScope.launch { viewModel.navigator.toAddContactDetail() }
 //        }
     }
 
