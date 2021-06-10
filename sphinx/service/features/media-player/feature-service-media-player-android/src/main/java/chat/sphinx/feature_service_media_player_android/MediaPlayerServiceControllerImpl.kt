fcf6724f816d54f46d6de2e554f92021a02d4d76
Matthew Nelson <developer@matthewnelson.io>
2021-06-10 10:44:14 -0400

    stub out ServiceConnection and Binder

--- sphinx/service/features/media-player/feature-service-media-player-android/src/main/java/chat/sphinx/feature_service_media_player_android/MediaPlayerServiceControllerImpl.kt
+++ sphinx/service/features/media-player/feature-service-media-player-android/src/main/java/chat/sphinx/feature_service_media_player_android/MediaPlayerServiceControllerImpl.kt
@@ -1,19 +1,51 @@
 package chat.sphinx.feature_service_media_player_android
 
+import android.content.ComponentName
+import android.content.ServiceConnection
+import android.os.IBinder
 import chat.sphinx.concept_service_media.MediaPlayerServiceController
 import chat.sphinx.concept_service_media.MediaPlayerServiceState
+import chat.sphinx.feature_service_media_player_android.service.MediaPlayerService
 import io.matthewnelson.concept_coroutines.CoroutineDispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 
 internal class MediaPlayerServiceControllerImpl(
     dispatchers: CoroutineDispatchers
 ): MediaPlayerServiceController(), CoroutineDispatchers by dispatchers {
 
-    override fun getCurrentState(): MediaPlayerServiceState {
-        // TODO: Retrieve MediaPlayer state from binder
-        return MediaPlayerServiceState.ServiceInactive
+    private val binder: MutableStateFlow<MediaPlayerService.MediaPlayerServiceBinder?> by lazy {
+        MutableStateFlow(null)
     }
 
+    override fun getCurrentState(): MediaPlayerServiceState {
+        return binder.value?.getCurrentState() ?: MediaPlayerServiceState.ServiceInactive
+    }
+
+    @JvmSynthetic
+    fun clearBinderReference() {
+        binder.value = null
+    }
+
+    @JvmSynthetic
     fun dispatchState(mediaPlayerServiceState: MediaPlayerServiceState) {
         listenerHandler.dispatch(mediaPlayerServiceState)
+    }
+
+    inner class MediaPlayerServiceConnection: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            if (service != null) {
+                binder.value = service as MediaPlayerService.MediaPlayerServiceBinder
+            } else {
+                clearBinderReference()
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            clearBinderReference()
+        }
+    }
+
+    private val connection: MediaPlayerServiceConnection by lazy {
+        MediaPlayerServiceConnection()
     }
 }
--- sphinx/service/features/media-player/feature-service-media-player-android/src/main/java/chat/sphinx/feature_service_media_player_android/service/MediaPlayerService.kt
+++ sphinx/service/features/media-player/feature-service-media-player-android/src/main/java/chat/sphinx/feature_service_media_player_android/service/MediaPlayerService.kt
@@ -3,8 +3,11 @@ package chat.sphinx.feature_service_media_player_android.service
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
+import chat.sphinx.concept_service_media.MediaPlayerServiceState
 import chat.sphinx.feature_service_media_player_android.MediaPlayerServiceControllerImpl
+import chat.sphinx.wrapper_common.dashboard.ChatId
 
 internal abstract class MediaPlayerService: Service() {
 
@@ -12,7 +15,18 @@ internal abstract class MediaPlayerService: Service() {
 
     private var mediaPlayer: MediaPlayer? = null
 
+    inner class MediaPlayerServiceBinder: Binder() {
+        fun getCurrentState(): MediaPlayerServiceState.ServiceActive {
+            // TODO: Implement
+            return MediaPlayerServiceState.ServiceActive.Playing(ChatId(0), 0, 0)
+        }
+    }
+
+    private val binder: MediaPlayerServiceBinder by lazy {
+        MediaPlayerServiceBinder()
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+        return binder
     }
 }
