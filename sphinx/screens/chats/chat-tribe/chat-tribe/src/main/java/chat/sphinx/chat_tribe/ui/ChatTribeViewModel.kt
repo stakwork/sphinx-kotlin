e9646d6932a74cbcea3fcafe49a32e0d8d50eed0
Matthew Nelson <developer@matthewnelson.io>
2021-06-14 06:17:10 -0400

    remove unnecessary null checks

--- sphinx/screens/chats/chat-tribe/chat-tribe/src/main/java/chat/sphinx/chat_tribe/ui/ChatTribeViewModel.kt
+++ sphinx/screens/chats/chat-tribe/chat-tribe/src/main/java/chat/sphinx/chat_tribe/ui/ChatTribeViewModel.kt
@@ -228,22 +228,20 @@ internal class ChatTribeViewModel @Inject constructor(
     fun playEpisode(episode: PodcastEpisode, startTime: Int) {
         viewModelScope.launch(mainImmediate) {
             chatRepository.getChatById(args.chatId).firstOrNull()?.let { chat ->
-                chat?.let { chat ->
-                    podcast?.let { podcast ->
-                        withContext(io) {
-                            podcast.didStartPlayingEpisode(episode, startTime)
-                        }
-
-                        mediaPlayerServiceController.submitAction(
-                            UserAction.ServiceAction.Play(
-                                chat.id,
-                                episode.id,
-                                episode.enclosureUrl,
-                                Sat(0),
-                                startTime,
-                            )
-                        )
+                podcast?.let { podcast ->
+                    withContext(io) {
+                        podcast.didStartPlayingEpisode(episode, startTime)
                     }
+
+                    mediaPlayerServiceController.submitAction(
+                        UserAction.ServiceAction.Play(
+                            chat.id,
+                            episode.id,
+                            episode.enclosureUrl,
+                            Sat(0),
+                            startTime,
+                        )
+                    )
                 }
             }
         }
@@ -252,17 +250,15 @@ internal class ChatTribeViewModel @Inject constructor(
     fun pauseEpisode(episode: PodcastEpisode) {
         viewModelScope.launch(mainImmediate) {
             chatRepository.getChatById(args.chatId).firstOrNull()?.let { chat ->
-                chat?.let { chat ->
-                    podcast?.let { podcast ->
-                        podcast.didPausePlayingEpisode(episode)
+                podcast?.let { podcast ->
+                    podcast.didPausePlayingEpisode(episode)
 
-                        mediaPlayerServiceController.submitAction(
-                            UserAction.ServiceAction.Pause(
-                                chat.id,
-                                episode.id,
-                            )
+                    mediaPlayerServiceController.submitAction(
+                        UserAction.ServiceAction.Pause(
+                            chat.id,
+                            episode.id,
                         )
-                    }
+                    )
                 }
             }
         }
@@ -271,19 +267,17 @@ internal class ChatTribeViewModel @Inject constructor(
     fun seekTo(time: Int) {
         viewModelScope.launch(mainImmediate) {
             chatRepository.getChatById(args.chatId).firstOrNull()?.let { chat ->
-                chat?.let { chat ->
-                    podcast?.let { podcast ->
-                        podcast.didSeekTo(time)
+                podcast?.let { podcast ->
+                    podcast.didSeekTo(time)
 
-                        val metaData = podcast.getMetaData()
+                    val metaData = podcast.getMetaData()
 
-                        mediaPlayerServiceController.submitAction(
-                            UserAction.ServiceAction.Seek(
-                                chat.id,
-                                metaData
-                            )
+                    mediaPlayerServiceController.submitAction(
+                        UserAction.ServiceAction.Seek(
+                            chat.id,
+                            metaData
                         )
-                    }
+                    )
                 }
             }
         }
