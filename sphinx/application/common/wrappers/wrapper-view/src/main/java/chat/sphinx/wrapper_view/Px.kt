5d20fe510a26eb7f88279e29941104d6a277e753
Matthew Nelson <developer@matthewnelson.io>
2021-06-17 13:19:04 -0400

    add extension functions for dividing/multiplying by Floats

--- sphinx/application/common/wrappers/wrapper-view/src/main/java/chat/sphinx/wrapper_view/Px.kt
+++ sphinx/application/common/wrappers/wrapper-view/src/main/java/chat/sphinx/wrapper_view/Px.kt
@@ -13,8 +13,17 @@ inline fun Px.multiplyBy(px: Px): Px =
     Px(value * px.value)
 
 @Suppress("NOTHING_TO_INLINE")
+inline fun Px.multiplyBy(float: Float): Px =
+    Px(value * float)
+
+@Suppress("NOTHING_TO_INLINE")
 inline fun Px.divideBy(px: Px): Px =
     Px(value / px.value)
+
+@Throws(ArithmeticException::class)
+@Suppress("NOTHING_TO_INLINE")
+inline fun Px.divideBy(float: Float): Px =
+    Px(value / float)
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun Px.isGreaterThanOrEqualTo(px: Px): Boolean =
