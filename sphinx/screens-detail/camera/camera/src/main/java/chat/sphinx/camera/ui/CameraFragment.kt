59d4778dfd8630aa6ab67d547b4d41d005459893
Matthew Nelson <developer@matthewnelson.io>
2021-06-24 13:54:27 -0400

    implement camera permission checking on screen start up

--- sphinx/screens-detail/camera/camera/src/main/java/chat/sphinx/camera/ui/CameraFragment.kt
+++ sphinx/screens-detail/camera/camera/src/main/java/chat/sphinx/camera/ui/CameraFragment.kt
@@ -1,13 +1,29 @@
 package chat.sphinx.camera.ui
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import chat.sphinx.camera.R
 import chat.sphinx.camera.databinding.FragmentCameraBinding
+import chat.sphinx.insetter_activity.InsetterActivity
+import chat.sphinx.insetter_activity.addNavigationBarPadding
 import dagger.hilt.android.AndroidEntryPoint
 import io.matthewnelson.android_feature_screens.ui.sideeffect.SideEffectFragment
+import io.matthewnelson.android_feature_viewmodel.submitSideEffect
+import kotlinx.coroutines.launch
+
+private val PERMISSIONS_REQUIRED = arrayOf(
+    Manifest.permission.CAMERA,
+    Manifest.permission.RECORD_AUDIO,
+)
 
 @AndroidEntryPoint
 internal class CameraFragment: SideEffectFragment<
@@ -21,13 +37,56 @@ internal class CameraFragment: SideEffectFragment<
     override val binding: FragmentCameraBinding by viewBinding(FragmentCameraBinding::bind)
     override val viewModel: CameraViewModel by viewModels()
 
+    private val requestPermissionLauncher by lazy(LazyThreadSafetyMode.NONE) {
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { response ->
+
+            try {
+                for (permission in PERMISSIONS_REQUIRED) {
+                    if (response[permission] != true) {
+                        throw Exception()
+                    }
+                }
+
+                startCamera()
+            } catch (e: Exception) {
+                lifecycleScope.launch(viewModel.mainImmediate) {
+                    viewModel.submitSideEffect(
+                        CameraSideEffect.Notify(getString(R.string.camera_permissions_required))
+                    )
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (requireActivity() as InsetterActivity).addNavigationBarPadding(binding.root)
+
+        if (hasPermissions(requireContext())) {
+            startCamera()
+        } else {
+            requestPermissionLauncher.launch(PERMISSIONS_REQUIRED)
+        }
+    }
+
+    private fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        // TODO: Implement
+    }
+
     override suspend fun onSideEffectCollect(sideEffect: CameraSideEffect) {
-//        TODO("Not yet implemented")
+        sideEffect.execute(requireActivity())
     }
 
     override suspend fun onViewStateFlowCollect(viewState: CameraViewState) {
