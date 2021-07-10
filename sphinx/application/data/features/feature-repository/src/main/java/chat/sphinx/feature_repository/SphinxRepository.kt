864abc96f5190e53830bb18c2fbfd3d58040875d
Kgothatso Ngako <kpangako@gmail.com>
2021-07-10 05:39:12 +0200

    Add Giphy integration

--- sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/SphinxRepository.kt
+++ sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/SphinxRepository.kt
@@ -29,8 +29,8 @@ import chat.sphinx.concept_repository_message.model.AttachmentInfo
 import chat.sphinx.concept_repository_message.model.SendMessage
 import chat.sphinx.concept_repository_message.model.SendPayment
 import chat.sphinx.concept_socket_io.SocketIOManager
-import chat.sphinx.concept_socket_io.SphinxSocketIOMessageListener
 import chat.sphinx.concept_socket_io.SphinxSocketIOMessage
+import chat.sphinx.concept_socket_io.SphinxSocketIOMessageListener
 import chat.sphinx.conceptcoredb.*
 import chat.sphinx.feature_repository.mappers.chat.ChatDboPresenterMapper
 import chat.sphinx.feature_repository.mappers.contact.ContactDboPresenterMapper
@@ -54,7 +54,6 @@ import chat.sphinx.wrapper_common.dashboard.ContactId
 import chat.sphinx.wrapper_common.dashboard.InviteId
 import chat.sphinx.wrapper_common.dashboard.toChatId
 import chat.sphinx.wrapper_common.invite.InviteStatus
-import chat.sphinx.wrapper_common.invite.toInviteStatus
 import chat.sphinx.wrapper_common.lightning.LightningNodePubKey
 import chat.sphinx.wrapper_common.lightning.LightningRouteHint
 import chat.sphinx.wrapper_common.lightning.Sat
@@ -89,8 +88,8 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import okio.base64.encodeBase64
 import java.text.ParseException
-import kotlin.collections.ArrayList
 import kotlin.math.absoluteValue
 
 abstract class SphinxRepository(
@@ -1219,6 +1218,12 @@ abstract class SphinxRepository(
 
     private val provisionalMessageLock = Mutex()
 
+    private fun messageText(sendMessage: SendMessage, moshi: Moshi): String? {
+        return sendMessage.giphyData?.let{
+            "giphy::${it.toJson(moshi).toByteArray().encodeBase64()}"
+        } ?: sendMessage.text
+    }
+
     // TODO: Rework to handle different message types
     @OptIn(RawPasswordAccess::class)
     override fun sendMessage(sendMessage: SendMessage?) {
@@ -1270,8 +1275,7 @@ abstract class SphinxRepository(
             }
 
             // encrypt text
-            val message: Pair<MessageContentDecrypted, MessageContent>? = sendMessage.text?.let { msgText ->
-
+            val message: Pair<MessageContentDecrypted, MessageContent>? = messageText(sendMessage, moshi)?.let { msgText ->
 
                 val response = rsa.encrypt(
                     ownerPubKey,
@@ -1296,26 +1300,30 @@ abstract class SphinxRepository(
             }
 
             // media attachment
-            val media: Triple<Password, MediaKey, AttachmentInfo>? = sendMessage.attachmentInfo?.let { info ->
-                val password = PasswordGenerator(MEDIA_KEY_SIZE).password
+            val media: Triple<Password, MediaKey, AttachmentInfo>? = if (sendMessage.giphyData == null) {
+                sendMessage.attachmentInfo?.let { info ->
+                    val password = PasswordGenerator(MEDIA_KEY_SIZE).password
 
-                val response = rsa.encrypt(
-                    ownerPubKey,
-                    UnencryptedString(password.value.joinToString("")),
-                    formatOutput = false,
-                    dispatcher = default,
-                )
+                    val response = rsa.encrypt(
+                        ownerPubKey,
+                        UnencryptedString(password.value.joinToString("")),
+                        formatOutput = false,
+                        dispatcher = default,
+                    )
 
-                @Exhaustive
-                when (response) {
-                    is Response.Error -> {
-                        LOG.e(TAG, response.message, response.exception)
-                        null
-                    }
-                    is Response.Success -> {
-                        Triple(password, MediaKey(response.value.value), info)
+                    @Exhaustive
+                    when (response) {
+                        is Response.Error -> {
+                            LOG.e(TAG, response.message, response.exception)
+                            null
+                        }
+                        is Response.Success -> {
+                            Triple(password, MediaKey(response.value.value), info)
+                        }
                     }
                 }
+            } else {
+                null
             }
 
             if (message == null && media == null) {
