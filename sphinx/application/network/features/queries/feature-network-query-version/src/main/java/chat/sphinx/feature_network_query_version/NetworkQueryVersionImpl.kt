8a90ff10b28675ce85cd8dc0f4b975b6391f17cc
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2021-08-03 16:50:30 -0300

    Changes related with app upgrade button

---
+++ sphinx/application/network/features/queries/feature-network-query-version/src/main/java/chat/sphinx/feature_network_query_version/NetworkQueryVersionImpl.kt
@@ -0,0 +1,29 @@
+package chat.sphinx.feature_network_query_version
+
+import chat.sphinx.concept_network_query_version.model.AppVersionsDto
+import chat.sphinx.concept_network_query_version.NetworkQueryVersion
+import chat.sphinx.concept_network_relay_call.NetworkRelayCall
+import chat.sphinx.feature_network_query_version.model.GetAppVersionsRelayResponse
+import chat.sphinx.kotlin_response.LoadResponse
+import chat.sphinx.kotlin_response.ResponseError
+import chat.sphinx.wrapper_relay.AuthorizationToken
+import chat.sphinx.wrapper_relay.RelayUrl
+import kotlinx.coroutines.flow.Flow
+
+class NetworkQueryVersionImpl(
+    private val networkRelayCall: NetworkRelayCall,
+): NetworkQueryVersion() {
+
+    companion object {
+        private const val ENDPOINT_APP_VERSIONS = "/app_versions"
+    }
+
+    override fun getAppVersions(
+        relayData: Pair<AuthorizationToken, RelayUrl>?
+    ): Flow<LoadResponse<AppVersionsDto, ResponseError>> =
+        networkRelayCall.relayGet(
+            responseJsonClass = GetAppVersionsRelayResponse::class.java,
+            relayEndpoint = ENDPOINT_APP_VERSIONS,
+            relayData = relayData
+        )
+}
---
+++ sphinx/application/network/features/queries/feature-network-query-version/src/test/java/chat/sphinx/feature_network_query_version/NetworkQueryVersionImplUnitTest.kt
@@ -0,0 +1,40 @@
+package chat.sphinx.feature_network_query_version
+
+import app.cash.exhaustive.Exhaustive
+import chat.sphinx.kotlin_response.LoadResponse
+import chat.sphinx.kotlin_response.Response
+import chat.sphinx.kotlin_response.exception
+import chat.sphinx.kotlin_response.message
+import chat.sphinx.test_network_query.NetworkQueryTestHelper
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class NetworkQueryVersionImplUnitTest: NetworkQueryTestHelper() {
+
+    @Test
+    fun `getAppVersions returns success`() =
+        testDispatcher.runBlockingTest {
+            getCredentials()?.let {
+                nqVersion.getAppVersions().collect { loadResponse ->
+
+                    @Exhaustive
+                    when (loadResponse) {
+                        is Response.Error -> {
+                            // will fail on error
+                            loadResponse.exception?.printStackTrace()
+                            Assert.fail(loadResponse.message)
+                        }
+                        is Response.Success -> {
+                        }
+                        is LoadResponse.Loading -> {
+                        }
+                    }
+
+                }
+            }
+        }
+}
