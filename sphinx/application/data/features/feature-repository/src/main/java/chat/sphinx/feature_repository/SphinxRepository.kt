bf4fe40dc82996643726060943f2f169f600c51e
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2021-08-10 10:10:10 -0300

    Delay for socket attachments removed

--- sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/SphinxRepository.kt
+++ sphinx/application/data/features/feature-repository/src/main/java/chat/sphinx/feature_repository/SphinxRepository.kt
@@ -217,9 +217,6 @@ abstract class SphinxRepository(
                             io
                         )?.join()
 
-                        val isAttachmentMessage = nnMessageDto.type.toMessageType().isAttachment()
-                        delay(if (isAttachmentMessage) 500L else 0L)
-
                         chatLock.withLock {
                             messageLock.withLock {
                                 contactLock.withLock {
