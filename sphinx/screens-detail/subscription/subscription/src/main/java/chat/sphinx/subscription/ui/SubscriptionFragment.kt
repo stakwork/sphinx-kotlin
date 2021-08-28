06fdcd344540587389cb088fa5a47c8e166a92f8
Kgothatso Ngako <kpangako+github@gmail.com>
2021-08-28 23:21:49 +0200

    Created custom radio group that extends constraint layout
    Also extract strings and handling interactions

--- sphinx/screens-detail/subscription/subscription/src/main/java/chat/sphinx/subscription/ui/SubscriptionFragment.kt
+++ sphinx/screens-detail/subscription/subscription/src/main/java/chat/sphinx/subscription/ui/SubscriptionFragment.kt
@@ -1,16 +1,24 @@
 package chat.sphinx.subscription.ui
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
+import chat.sphinx.insetter_activity.InsetterActivity
+import chat.sphinx.insetter_activity.addNavigationBarPadding
 import chat.sphinx.subscription.R
 import chat.sphinx.subscription.databinding.FragmentSubscriptionBinding
 import dagger.hilt.android.AndroidEntryPoint
 import io.matthewnelson.android_feature_screens.ui.base.BaseFragment
 import io.matthewnelson.android_feature_screens.util.visible
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 @AndroidEntryPoint
 internal class SubscriptionFragment: BaseFragment<
@@ -35,6 +43,96 @@ internal class SubscriptionFragment: BaseFragment<
                     lifecycleScope.launch { viewModel.navigator.popBackStack() }
                 }
             }
+        }
+
+        binding.apply {
+            val calendar = Calendar.getInstance()
+
+            editTextPayUntil.setOnClickListener {
+                this@SubscriptionFragment.context?.let { context ->
+                    val datePickerDialog = DatePickerDialog(
+                        context,
+                        { _, year, month, dayOfMonth ->
+                            calendar.set(Calendar.YEAR, year)
+                            calendar.set(Calendar.MONTH, month)
+                            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                            editTextPayUntil.setText(
+                                SimpleDateFormat("dd/MM/yyyy", Locale.US).format(calendar.time)
+                            )
+                        },
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH),
+                    )
+                    datePickerDialog.datePicker.minDate = calendar.timeInMillis
+                    datePickerDialog.show()
+                }
+
+            }
+
+            radioButtonCustomAmount.setOnCheckedChangeListener { _, isChecked ->
+                editTextCustomAmount.isEnabled = isChecked
+            }
+
+            radioButtonMake.setOnCheckedChangeListener { _, isChecked ->
+                editTextMakeQuantity.isEnabled = isChecked
+            }
+
+            radioButtonUntil.setOnCheckedChangeListener { _, isChecked ->
+                editTextPayUntil.isEnabled = isChecked
+            }
+
+            buttonSave.setOnClickListener {
+
+                val amount: Int? = when {
+                    radioGroupAmount.checkedRadioButtonId == R.id.radio_button_500_sats -> {
+                        500
+                    }
+                    radioGroupAmount.checkedRadioButtonId == R.id.radio_button_1000_sats -> {
+                        1000
+                    }
+                    radioGroupAmount.checkedRadioButtonId == R.id.radio_button_2000_sats -> {
+                        2000
+                    }
+                    radioGroupAmount.checkedRadioButtonId == R.id.radio_button_custom_amount -> {
+                        editTextCustomAmount.text?.toString()?.toIntOrNull()
+                    }
+                    else -> null
+                }
+
+                val cron: String? = when {
+                    radioGroupTimeInterval.checkedRadioButtonId == R.id.radio_button_daily -> {
+                        "daily"
+                    }
+                    radioGroupTimeInterval.checkedRadioButtonId == R.id.radio_button_weekly -> {
+                        "weekly"
+                    }
+                    radioGroupTimeInterval.checkedRadioButtonId == R.id.radio_button_monthly -> {
+                        "monthly"
+                    }
+                    else -> null
+                }
+
+                val endDate: Date? = when {
+                    radioGroupEndRule.checkedRadioButtonId == R.id.radio_button_make -> {
+                        // TODO: Calculate depending on the timeInterval
+                        Calendar.getInstance().time
+                    }
+                    radioGroupEndRule.checkedRadioButtonId == R.id.radio_button_until -> {
+                        // TODO: Load editTextPayUntil.text into date
+                        Calendar.getInstance().time
+                    }
+                    else -> null
+                }
+
+                viewModel.saveSubscription(
+                    amount,
+                    cron,
+                    endDate
+                )
+            }
+            (requireActivity() as InsetterActivity).addNavigationBarPadding(layoutConstraintSubscription)
         }
     }
 
--- sphinx/screens-detail/subscription/subscription/src/main/java/chat/sphinx/subscription/ui/SubscriptionViewModel.kt
+++ sphinx/screens-detail/subscription/subscription/src/main/java/chat/sphinx/subscription/ui/SubscriptionViewModel.kt
@@ -4,6 +4,7 @@ import chat.sphinx.subscription.navigation.SubscriptionNavigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.matthewnelson.android_feature_viewmodel.BaseViewModel
 import io.matthewnelson.concept_coroutines.CoroutineDispatchers
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +13,20 @@ internal class SubscriptionViewModel @Inject constructor(
     val navigator: SubscriptionNavigator
 ): BaseViewModel<SubscriptionViewState>(dispatchers, SubscriptionViewState.Idle)
 {
+
+    fun saveSubscription(
+        amount: Int?,
+        cron: String?,
+        endDate: Date?
+    ) {
+        if (amount == null) {
+            // TODO: Let user know amount is required
+            return
+        }
+
+        if (cron == null) {
+            // TODO: Let user know interval is required
+            return
+        }
+    }
 }
