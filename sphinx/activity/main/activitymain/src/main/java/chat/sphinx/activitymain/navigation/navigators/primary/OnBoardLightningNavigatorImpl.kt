281000c8b40b2805d88c0a93ba12b2287f21355e
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2021-10-07 15:19:26 -0300

    Fix for OnBoardLightning screen moving forward to same screen

--- sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/navigation/navigators/primary/OnBoardLightningNavigatorImpl.kt
+++ sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/navigation/navigators/primary/OnBoardLightningNavigatorImpl.kt
@@ -5,6 +5,7 @@ import chat.sphinx.activitymain.navigation.drivers.PrimaryNavigationDriver
 import chat.sphinx.onboard_common.model.OnBoardStep
 import chat.sphinx.onboard_lightning.navigation.OnBoardLightningNavigator
 import chat.sphinx.onboard_lightning.navigation.ToOnBoardLightningScreen
+import chat.sphinx.onboard_name.navigation.ToOnBoardNameScreen
 import javax.inject.Inject
 
 internal class OnBoardLightningNavigatorImpl @Inject constructor(
@@ -13,7 +14,7 @@ internal class OnBoardLightningNavigatorImpl @Inject constructor(
 
     override suspend fun toOnBoardNameScreen(onBoardStep2: OnBoardStep.Step2_Name) {
         navigationDriver.submitNavigationRequest(
-            ToOnBoardLightningScreen(popUpToId = R.id.main_primary_nav_graph, onBoardStep2)
+            ToOnBoardNameScreen(popUpToId = R.id.main_primary_nav_graph, onBoardStep2)
         )
     }
 
