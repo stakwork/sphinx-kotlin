4c65edb4eb20d60216e6d8e1438147351470cd31
Kgothatso Ngako <kpangako+github@gmail.com>
2021-11-15 17:24:50 +0200

    Add newsletter feed adapter

--- sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/feed/read/FeedReadViewModel.kt
+++ sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/feed/read/FeedReadViewModel.kt
@@ -2,24 +2,48 @@ package chat.sphinx.dashboard.ui.feed.read
 
 import android.content.Context
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import chat.sphinx.concept_repository_dashboard_android.RepositoryDashboardAndroid
 import chat.sphinx.dashboard.navigation.DashboardNavigator
+import chat.sphinx.dashboard.ui.feed.FeedFollowingViewModel
 import chat.sphinx.dashboard.ui.viewstates.FeedReadViewState
+import chat.sphinx.wrapper_feed.Feed
+import chat.sphinx.wrapper_feed.FeedItem
+import chat.sphinx.wrapper_feed.FeedType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.matthewnelson.android_feature_viewmodel.SideEffectViewModel
 import io.matthewnelson.concept_coroutines.CoroutineDispatchers
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 
 @HiltViewModel
-internal class FeedReadViewModel @Inject constructor(
+class FeedReadViewModel @Inject constructor(
     handler: SavedStateHandle,
     val dashboardNavigator: DashboardNavigator,
+    private val repositoryDashboard: RepositoryDashboardAndroid<Any>,
     dispatchers: CoroutineDispatchers,
 ): SideEffectViewModel<
         Context,
         FeedReadSideEffect,
         FeedReadViewState
-        >(dispatchers, FeedReadViewState.Idle)
+        >(dispatchers, FeedReadViewState.Idle), FeedFollowingViewModel
 {
+    override val feedsHolderViewStateFlow: StateFlow<List<Feed>> = flow {
+        repositoryDashboard.getAllFeedsOfType(FeedType.Newsletter).collect { podcastFeeds ->
+            emit(podcastFeeds.toList())
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        emptyList()
+    )
 
+    override fun feedSelected(feed: Feed) {
+        // TODO: Handle feed navigation...
+    }
+
+    fun newsletterItemSelected(episode: FeedItem) {
+        // TODO: Handle the feedItem navigation
+    }
 }
