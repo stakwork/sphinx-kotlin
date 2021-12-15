c3f8af570d815e1d0366fdeb5da43a9eb6008555
Kgothatso Ngako <kpangako+github@gmail.com>
2021-12-15 19:46:08 +0200

    Fix for construction of video feed viewModels

--- sphinx/screens-detail/video-screen/video-screen/src/main/java/chat/sphinx/video_screen/ui/detail/VideoFeedDetailScreenViewModel.kt
+++ sphinx/screens-detail/video-screen/video-screen/src/main/java/chat/sphinx/video_screen/ui/detail/VideoFeedDetailScreenViewModel.kt
@@ -3,9 +3,8 @@ package chat.sphinx.video_screen.ui.detail
 import androidx.lifecycle.SavedStateHandle
 import chat.sphinx.concept_repository_chat.ChatRepository
 import chat.sphinx.concept_repository_feed.FeedRepository
+import chat.sphinx.concept_repository_media.RepositoryMedia
 import chat.sphinx.video_screen.ui.VideoFeedScreenViewModel
-import chat.sphinx.video_screen.ui.watch.chatId
-import chat.sphinx.video_screen.ui.watch.feedUrl
 import chat.sphinx.wrapper_common.dashboard.ChatId
 import chat.sphinx.wrapper_common.feed.FeedUrl
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -24,10 +23,12 @@ internal class VideoFeedDetailScreenViewModel @Inject constructor(
     dispatchers: CoroutineDispatchers,
     savedStateHandle: SavedStateHandle,
     chatRepository: ChatRepository,
+    repositoryMedia: RepositoryMedia,
     feedRepository: FeedRepository,
 ): VideoFeedScreenViewModel(
     dispatchers,
     chatRepository,
+    repositoryMedia,
     feedRepository
 )
 {
