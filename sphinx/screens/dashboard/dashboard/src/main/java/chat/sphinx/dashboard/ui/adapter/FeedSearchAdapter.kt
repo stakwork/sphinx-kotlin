2e85dc00b1232f99e4192e1893c5b2c4e62acd80
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2022-01-03 12:59:31 -0300

    Fixes related with blank images on feed

--- sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/adapter/FeedSearchAdapter.kt
+++ sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/adapter/FeedSearchAdapter.kt
@@ -186,9 +186,9 @@ class FeedSearchAdapter(
         }
     }
 
-    private val imageLoaderOptions: ImageLoaderOptions by lazy {
-        ImageLoaderOptions.Builder()
-            .placeholderResId(R.drawable.ic_podcast_placeholder)
+    private fun imageLoaderOptions(result: FeedSearchResult): ImageLoaderOptions {
+        return ImageLoaderOptions.Builder()
+            .placeholderResId(result.getPlaceHolderImageRes())
             .build()
     }
 
