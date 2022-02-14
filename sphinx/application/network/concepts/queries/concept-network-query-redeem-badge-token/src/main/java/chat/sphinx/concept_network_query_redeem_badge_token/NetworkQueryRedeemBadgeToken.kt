432a34ca58bd2121944bdbc3900cb47c939d68fc
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-02-14 10:34:38 -0600

    deleted getRedeemBadgeTOkenDto

--- sphinx/application/network/concepts/queries/concept-network-query-redeem-badge-token/src/main/java/chat/sphinx/concept_network_query_redeem_badge_token/NetworkQueryRedeemBadgeToken.kt
+++ sphinx/application/network/concepts/queries/concept-network-query-redeem-badge-token/src/main/java/chat/sphinx/concept_network_query_redeem_badge_token/NetworkQueryRedeemBadgeToken.kt
@@ -1,7 +1,6 @@
 package chat.sphinx.concept_network_query_redeem_badge_token
 
 import chat.sphinx.concept_network_query_redeem_badge_token.model.RedeemBadgeTokenDto
-import chat.sphinx.concept_network_query_redeem_badge_token.model.GetRedeemBadgeTokenDto
 import chat.sphinx.kotlin_response.LoadResponse
 import chat.sphinx.kotlin_response.ResponseError
 import chat.sphinx.wrapper_relay.AuthorizationToken
