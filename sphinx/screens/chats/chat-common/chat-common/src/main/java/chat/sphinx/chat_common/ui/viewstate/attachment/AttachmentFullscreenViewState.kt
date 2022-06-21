ec9a6e527222fe3992642d8bd01da0d379c0202c
Alejandro <ariel10aguero@gmail.com>
2022-06-21 19:28:26 -0300

    first commit

--- sphinx/screens/chats/chat-common/chat-common/src/main/java/chat/sphinx/chat_common/ui/viewstate/attachment/AttachmentFullscreenViewState.kt
+++ sphinx/screens/chats/chat-common/chat-common/src/main/java/chat/sphinx/chat_common/ui/viewstate/attachment/AttachmentFullscreenViewState.kt
@@ -3,7 +3,8 @@ package chat.sphinx.chat_common.ui.viewstate.attachment
 import chat.sphinx.wrapper_message_media.MessageMedia
 import io.matthewnelson.concept_views.viewstate.ViewState
 
-internal sealed class AttachmentFullscreenViewState: ViewState<AttachmentFullscreenViewState>() {
+internal sealed class
+AttachmentFullscreenViewState: ViewState<AttachmentFullscreenViewState>() {
 
     object Idle: AttachmentFullscreenViewState()
 
