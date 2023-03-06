74a83b980d514359301e6ae80ba7b98454c37bd0
Alejandro <ariel10aguero@gmail.com>
2023-03-06 14:32:36 -0300

    Set quantity on create badge

--- sphinx/screens-detail/create-badge/create-badge/src/main/java/chat/sphinx/create_badge/ui/CreateBadgeFragment.kt
+++ sphinx/screens-detail/create-badge/create-badge/src/main/java/chat/sphinx/create_badge/ui/CreateBadgeFragment.kt
@@ -2,8 +2,17 @@ package chat.sphinx.create_badge.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -13,6 +22,8 @@ import chat.sphinx.concept_image_loader.Transformation
 import chat.sphinx.create_badge.R
 import chat.sphinx.create_badge.databinding.FragmentCreateBadgeBinding
 import chat.sphinx.resources.getString
+import chat.sphinx.resources.inputMethodManager
+import chat.sphinx.wrapper_common.lightning.Sat
 import dagger.hilt.android.AndroidEntryPoint
 import io.matthewnelson.android_feature_screens.ui.sideeffect.SideEffectFragment
 import io.matthewnelson.android_feature_screens.util.gone
@@ -37,6 +48,8 @@ internal class CreateBadgeFragment: SideEffectFragment<
     @Suppress("ProtectedInFinal")
     protected lateinit var imageLoader: ImageLoader<ImageView>
 
+    private var currentQuantity = 100
+    private val pricePerBadge = 10
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -46,7 +59,6 @@ internal class CreateBadgeFragment: SideEffectFragment<
                 viewModel.navigator.popBackStack()
             }
         }
-
     }
 
     override suspend fun onViewStateFlowCollect(viewState: CreateBadgeViewState) {
@@ -99,12 +111,66 @@ internal class CreateBadgeFragment: SideEffectFragment<
                     }
                     textViewBadgeName.text = viewState.badgeTemplate.name
                     textViewBadgesRequirementDescription.text = viewState.badgeTemplate.description
+
+                   layoutConstraintCreateBadge.apply {
+                        removeFocusOnEnter(quantityNumber)
+                    }
+
+
+                    buttonBadgesQuantityMinus.setOnClickListener { decreaseQuantity() }
+                    buttonBadgesQuantityPlus.setOnClickListener { increaseQuantity() }
+
+                    setSatsQuantity()
                 }
 
             }
         }
 
     }
+
+
+    private fun decreaseQuantity() {
+        if (currentQuantity > 0) {
+            currentQuantity -= 1
+            setSatsQuantity()
+        }
+    }
+
+    private fun increaseQuantity() {
+        currentQuantity += 1
+        setSatsQuantity()
+    }
+    private fun setSatsQuantity() {
+        binding.apply {
+            quantityNumber.setText(currentQuantity.toString())
+            textViewSatsPerBadge.text = pricePerBadge.toString()
+            textViewTotalSatsAmount.text = (currentQuantity * pricePerBadge).toString()
+        }
+    }
+
+
+    private fun removeFocusOnEnter(editText: EditText?) {
+        editText?.setOnEditorActionListener(object:
+            TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_DONE || event?.keyCode == KeyEvent.KEYCODE_ENTER) {
+                    editText.let { nnEditText ->
+                        currentQuantity = nnEditText.text.toString().toInt()
+                        setSatsQuantity()
+                        binding.root.context.inputMethodManager?.let { imm ->
+                            if (imm.isActive(nnEditText)) {
+                                imm.hideSoftInputFromWindow(nnEditText.windowToken, 0)
+                                nnEditText.clearFocus()
+                            }
+                        }
+                    }
+                    return true
+                }
+                return false
+            }
+        })
+    }
+
 
     override fun subscribeToViewStateFlow() {
         super.subscribeToViewStateFlow()
