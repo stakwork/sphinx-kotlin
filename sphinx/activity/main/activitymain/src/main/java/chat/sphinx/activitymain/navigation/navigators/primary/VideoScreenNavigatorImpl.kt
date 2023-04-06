8fa3a30d1e2df1710f0f05c03a6192a7665352df
Alejandro <ariel10aguero@gmail.com>
2023-04-06 17:29:51 -0300

    Navigate to screen description from Recommendation and Video

--- sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/navigation/navigators/primary/VideoScreenNavigatorImpl.kt
+++ sphinx/activity/main/activitymain/src/main/java/chat/sphinx/activitymain/navigation/navigators/primary/VideoScreenNavigatorImpl.kt
@@ -2,6 +2,7 @@ package chat.sphinx.activitymain.navigation.navigators.primary
 
 import chat.sphinx.activitymain.navigation.drivers.DetailNavigationDriver
 import chat.sphinx.activitymain.navigation.drivers.PrimaryNavigationDriver
+import chat.sphinx.episode_description.navigation.ToEpisodeDescription
 import chat.sphinx.episode_detail.navigation.ToEpisodeDetail
 import chat.sphinx.video_screen.navigation.VideoScreenNavigator
 import chat.sphinx.wrapper_common.feed.FeedId
@@ -16,6 +17,9 @@ internal class VideoScreenNavigatorImpl @Inject constructor(
         (navigationDriver as DetailNavigationDriver).closeDetailScreen()
     }
 
+    override suspend fun toEpisodeDescriptionScreen(feedId: FeedId) {
+        (navigationDriver as DetailNavigationDriver).submitNavigationRequest(ToEpisodeDescription(feedId))
+    }
     override suspend fun toEpisodeDetail(
         feedItemId: FeedId?,
         header: String,
