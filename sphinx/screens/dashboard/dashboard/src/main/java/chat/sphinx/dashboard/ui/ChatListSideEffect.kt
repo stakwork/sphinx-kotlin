57fb229ce4b709d0148bbd1e4b5c63c21d01c17c
Alejandro <ariel10aguero@gmail.com>
2023-06-29 17:53:38 -0300

    WIP handleLightningNodeLink

--- sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/ChatListSideEffect.kt
+++ sphinx/screens/dashboard/dashboard/src/main/java/chat/sphinx/dashboard/ui/ChatListSideEffect.kt
@@ -2,6 +2,10 @@ package chat.sphinx.dashboard.ui
 
 import android.app.AlertDialog
 import android.content.Context
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.FrameLayout
 import chat.sphinx.dashboard.R
 import chat.sphinx.resources.SphinxToastUtils
 import io.matthewnelson.android_feature_toast_utils.show
@@ -66,6 +70,120 @@ sealed class ChatListSideEffect: SideEffect<Context>() {
             builder.setNegativeButton(android.R.string.cancel) { _,_ -> }
             builder.setPositiveButton(android.R.string.ok) { _, _ ->
                 callback()
+            }
+            builder.show()
+        }
+    }
+
+    class CheckNetwork(
+        private val callback: () -> Unit,
+    ): ChatListSideEffect() {
+        override suspend fun execute(value: Context) {
+            val builder = AlertDialog.Builder(value, R.style.AlertDialogTheme)
+            builder.setTitle(value.getString(R.string.network_check_title))
+            builder.setMessage(value.getString(R.string.network_check_message))
+            builder.setNegativeButton(R.string.no) { _, _ -> }
+            builder.setPositiveButton(R.string.yes) { _, _ ->
+                callback.invoke()
+            }
+            builder.show()
+        }
+    }
+
+    class FailedToSetupSigningDevice(
+        private val errorMessage: String
+    ): ChatListSideEffect() {
+        override suspend fun execute(value: Context) {
+            SphinxToastUtils(true).show(
+                value,
+                value.getString(R.string.error_setting_up_signing_device, errorMessage)
+            )
+        }
+    }
+
+    class SigningDeviceInfo(
+        private val title: String,
+        private val message: String,
+        private val defaultValue: String? = null,
+        private val inputType: Int? = null,
+        private val callback: (String?) -> Unit,
+    ): ChatListSideEffect() {
+
+        override suspend fun execute(value: Context) {
+
+            val inputEditTextField = EditText(value)
+            inputEditTextField.isSingleLine = true
+            inputType?.let {
+                inputEditTextField.inputType = it
+            }
+            inputEditTextField.setOnFocusChangeListener { _, _ ->
+                inputEditTextField.post {
+                    val inputMethodManager: InputMethodManager =
+                        value.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.showSoftInput(inputEditTextField, InputMethodManager.SHOW_IMPLICIT)
+                }
+            }
+            inputEditTextField.requestFocus()
+            inputEditTextField.setText(defaultValue)
+
+            val container = FrameLayout(value)
+            val params: FrameLayout.LayoutParams = FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            params.leftMargin = value.resources.getDimensionPixelSize(R.dimen.default_layout_margin)
+            params.rightMargin = value.resources.getDimensionPixelSize(R.dimen.default_layout_margin)
+            inputEditTextField.layoutParams = params
+            container.addView(inputEditTextField)
+
+            val builder = AlertDialog.Builder(value, R.style.AlertDialogTheme)
+            builder.setTitle(title)
+            builder.setMessage(message)
+            builder.setView(container)
+            builder.setPositiveButton(android.R.string.ok) { _, _ ->
+                val editTextInput = inputEditTextField.text.toString()
+
+                callback.invoke(
+                    if (editTextInput.isEmpty()) {
+                        null
+                    } else {
+                        editTextInput
+                    }
+                )
+            }
+            builder.setNegativeButton(android.R.string.cancel) { _, _ -> }
+
+            builder.show()
+        }
+    }
+
+    class CheckBitcoinNetwork(
+        private val regTestCallback: () -> Unit,
+        private val mainNetCallback: () -> Unit,
+        private val callback: () -> Unit,
+    ): ProfileSideEffect() {
+        override suspend fun execute(value: Context) {
+            val builder = AlertDialog.Builder(value, R.style.AlertDialogTheme)
+            builder.setTitle(value.getString(R.string.select_bitcoin_network))
+
+            val items = arrayOf(BITCOIN_NETWORK_REG_TEST.toCapitalized(), BITCOIN_NETWORK_MAIN_NET.toCapitalized())
+            builder.setSingleChoiceItems(items, 0
+            ) { _, p1 ->
+                when (p1) {
+                    0 -> {
+                        regTestCallback.invoke()
+                    }
+                    1 -> {
+                        mainNetCallback.invoke()
+                    }
+                    else -> {}
+                }
+            }
+            builder.setNegativeButton(android.R.string.cancel) { dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.setPositiveButton(android.R.string.ok) { _, _ ->
+                callback.invoke()
             }
             builder.show()
         }
