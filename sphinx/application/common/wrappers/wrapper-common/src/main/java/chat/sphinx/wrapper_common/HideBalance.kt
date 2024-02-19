1aff5f743184358cd47da1730b0bad8d80e2e0a2
KotlinGeekDev <kotlingeek@protonmail.com>
2024-02-19 12:19:22 +0100

    Make HideBalance an object, as no sealed-class functionality is needed. This could be reverted if needed.

--- sphinx/application/common/wrappers/wrapper-common/src/main/java/chat/sphinx/wrapper_common/HideBalance.kt
+++ sphinx/application/common/wrappers/wrapper-common/src/main/java/chat/sphinx/wrapper_common/HideBalance.kt
@@ -1,24 +1,11 @@
 package chat.sphinx.wrapper_common
 
-sealed class HideBalance {
+object HideBalance {
 
-    companion object {
-        const val ENABLED = 1
-        const val DISABLED = 0
+    const val ENABLED = 1
+    const val DISABLED = 0
 
-        const val HIDE_BALANCE_SHARED_PREFERENCES = "general_settings"
-        const val HIDE_BALANCE_ENABLED_KEY = "hide-balance-enabled"
-    }
+    const val HIDE_BALANCE_SHARED_PREFERENCES = "general_settings"
+    const val HIDE_BALANCE_ENABLED_KEY = "hide-balance-enabled"
 
-    abstract val value: Int
-
-    object True: HideBalance(){
-        override val value: Int
-            get() = ENABLED
-    }
-
-    object False: HideBalance(){
-        override val value: Int
-            get() = DISABLED
-    }
 }
